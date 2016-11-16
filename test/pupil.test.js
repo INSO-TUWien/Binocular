e2a61844ab73383919172de3bbe3b9fc80dd7f7a
Roman Decker <roman.decker@gmail.com>
2016-11-16 17:22:37 +0100

    Added basic skeleton for tests & coverage
    Fixes #2

---
+++ test/pupil.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Promise = require( 'bluebird' );
+
+describe('should correctly parse the config', function() {
+  it('should work', function() {
+    return Promise.resolve();
+  });
+});
