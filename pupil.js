a3816559b93e73b589326643bbb57478a471772a
Roman Decker <roman.decker@gmail.com>
2016-11-23 13:56:47 +0100

    :sparkles: Added git into the mix
    Fixes #7

--- pupil.js
+++ pupil.js
@@ -3,3 +3,12 @@
 'use strict';
 
 require( './lib/context.js' );
+
+const _ = require( 'lodash' );
+const git = require( './lib/git.js' );
+
+git.getAllCommits( '.' )
+.map( function( commit ) {
+  const header = _.head( _.split(commit.message(), '\n', 2) );
+  console.log( `${commit.id()} ${header}` );
+} );
