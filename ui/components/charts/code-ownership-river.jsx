fec41a6fc3e72606a707201479fe8f246f8ac88a
Roman Decker <roman.decker@gmail.com>
2017-01-03 13:06:56 +0100

    :sparkles: Added basic chart components
    Issue: #13 - Code Ownership River

---
+++ ui/components/charts/code-ownership-river.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+import React, { PropTypes } from 'react';
+import _ from 'lodash';
+import styles from './code-ownserhip-river.css';
+
+import classnames from 'classnames';
+
+export default class CodeOwnershipRiver extends React.Component {
+  render() {
+
+    return (
+      <h1>Code Ownership River</h1>
+    );
+  }
+};
+
+CodeOwnershipRiver.propTypes = {};
---
+++ ui/components/charts/issue-impact.jsx
@@ -0,0 +1,18 @@
+'use strict';
+
+import React, { PropTypes } from 'react';
+import _ from 'lodash';
+import styles from './issue-impact.css';
+
+import classnames from 'classnames';
+
+export default class IssueImpact extends React.Component {
+  render() {
+
+    return (
+      <h1>Issue Impact</h1>
+    );
+  }
+};
+
+IssueImpact.propTypes = {};
