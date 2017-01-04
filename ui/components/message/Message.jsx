c600639942b857052410e7d693ea7152a16f41a0
Roman Decker <roman.decker@gmail.com>
2017-01-04 10:41:04 +0100

    :bug: Fixed handling of style attribute in Message component
    Issue: #15 - Create a configuration interface

--- ui/components/message/Message.jsx
+++ ui/components/message/Message.jsx
@@ -7,8 +7,11 @@ import cx from 'classnames';
 
 export default class Message extends React.Component {
   render() {
+    const hasClass = !!this.props.style;
+    const styleClass = bulma[`is-${this.props.style}`];
+    
     return (
-      <article className={cx(bulma.message, bulma[`is-${this.props.type || 'info'}`])}>
+      <article className={cx(bulma.message, { [styleClass]: hasClass })}>
         {this.props.header &&
           <div className={bulma['message-header']}>
             {this.props.header}
