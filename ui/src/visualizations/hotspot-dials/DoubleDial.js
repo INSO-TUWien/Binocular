6d63dbaf6cf9dcf419d5b355977b24b6c6dc3238
Roman Decker <roman.decker@gmail.com>
2017-12-12 14:24:04 +0100

    :lipstick: Fix display of good/bad commits
    Issue: #51 - Hotspot Dials

--- ui/src/visualizations/hotspot-dials/DoubleDial.js
+++ ui/src/visualizations/hotspot-dials/DoubleDial.js
@@ -22,128 +22,68 @@ export default function Dial(props) {
   const upperScale = scale.copy().range([midShare, MAXIMUM_FILL_RATE]);
   const lowerScale = scale.copy().range([midShare, props.minimumFillRate]);
 
+  const tickShare = 1 / props.categories.length;
+  const halfTickShare = tickShare / 2;
+
   const clock = new ClockScale(0, 0, props.radius);
-
-  const dataPoints = [];
-  const slices = [];
-
-  const upperPoints = [];
-  const lowerPoints = [];
-
-  const axisTextShare = -1 / props.categories.length / 2 + 1 / props.categories.length / 30;
-  const axisTextAngle = 180 + clock.getAngleForShare(axisTextShare) / Math.PI * 180;
-  const axisTextCoords = clock.getCoordsForShare(axisTextShare, midShare);
+  const elems = [];
+  const descriptionOffsetShare = (MAXIMUM_FILL_RATE - props.minimumFillRate) / 8;
 
   _.each(props.categories, (cat, i) => {
-    const upperCoords = clock.getCoordsForShare(
-      (cat.category - 1) / props.categories.length,
-      upperScale(cat.goodCount)
-    );
+    const centerShare = i / props.categories.length;
+    const leftShare = centerShare - halfTickShare;
+    const rightShare = leftShare + tickShare;
 
-    const lowerCoords = clock.getCoordsForShare(
-      (cat.category - 1) / props.categories.length,
-      lowerScale(cat.badCount)
-    );
+    const topLeft = clock.getCoordsForShare(leftShare);
+    const topRight = clock.getCoordsForShare(rightShare);
+    const sTopLeft = clock.getCoordsForShare(leftShare, upperScale(cat.goodCount));
+    const sTopRight = clock.getCoordsForShare(rightShare, upperScale(cat.goodCount));
+    const sBottomLeft = clock.getCoordsForShare(leftShare, lowerScale(cat.badCount));
+    const sBottomRight = clock.getCoordsForShare(rightShare, lowerScale(cat.badCount));
 
-    upperPoints.push(upperCoords);
-    lowerPoints.push(lowerCoords);
+    const slice = new ClosingPathContext();
+    slice.moveTo(sTopLeft.x, sTopLeft.y);
+    const upperArcRadius = props.radius * upperScale(cat.goodCount);
+    const lowerArcRadius = props.radius * lowerScale(cat.badCount);
+    slice.arcTo(upperArcRadius, upperArcRadius, 0, 0, 1, sTopRight.x, -sTopRight.y);
+    slice.lineTo(sBottomRight.x, sBottomRight.y);
+    slice.arcTo(lowerArcRadius, lowerArcRadius, 0, 0, 0, sBottomLeft.x, -sBottomLeft.y);
+    slice.lineTo(sTopLeft.x, sTopLeft.y);
 
-    const upperTextCoords = clock.getCoordsForShare(
-      (cat.category - 1) / props.categories.length,
-      midShare * 1.05
-    );
-    const lowerTextCoords = clock.getCoordsForShare(
-      (cat.category - 1) / props.categories.length,
-      midShare * 0.95
-    );
+    const descriptionTextCoords = clock.getCoordsForShare(centerShare, 1.05);
+    const outerTextCoords = clock.getCoordsForShare(centerShare, midShare + descriptionOffsetShare);
+    const innerTextCoords = clock.getCoordsForShare(centerShare, midShare - descriptionOffsetShare);
 
-    dataPoints.push(
-      <g key={i} className={styles.counts}>
-        <text x={upperTextCoords.x} y={upperTextCoords.y} className={styles.count}>
+    elems.push(<path d={slice} style={{ fill: props.palette[i] }} />);
+    elems.push(<line x1={0} y1={0} x2={topLeft.x} y2={topLeft.y} className={styles.tick} />);
+    elems.push(<line x1={0} y1={0} x2={topRight.x} y2={topRight.y} className={styles.tick} />);
+    elems.push(
+      <text x={descriptionTextCoords.x} y={descriptionTextCoords.y}>
+        {cat.label}
+      </text>
+    );
+    elems.push(
+      <g className={styles.counts}>
+        <text x={outerTextCoords.x} y={outerTextCoords.y}>
           {cat.goodCount}
         </text>
-        <text x={lowerTextCoords.x} y={lowerTextCoords.y} className={styles.count}>
+        <text x={innerTextCoords.x} y={innerTextCoords.y}>
           {cat.badCount}
         </text>
       </g>
     );
   });
 
-  const upperCurvePath = new ClosingPathContext();
-  upperCurvePath.smoothCurve(upperPoints, { smoothness: 0.5, close: true });
-
-  const lowerCurvePath = new ClosingPathContext();
-  lowerCurvePath.smoothCurve(lowerPoints, { smoothness: 0.5, close: true });
-
-  const tickMarks = props.categories.map((cat, i) => {
-    const textShare = i / props.categories.length;
-    const tickShare = textShare - 1 / (2 * props.categories.length);
-
-    const textCoords = clock.getCoordsForShare(textShare, 1.05);
-    const insetCoords = clock.getCoordsForShare(tickShare, props.minimumFillRate);
-    const faceCoords = clock.getCoordsForShare(tickShare);
-
-    const slice = new ClosingPathContext();
-    slice.moveTo(0, 0);
-    const topLeft = clock.getCoordsForShare(tickShare, 2);
-    const topRight = clock.getCoordsForShare(tickShare + 1 / props.categories.length, 1.1);
-    slice.lineTo(topLeft.x, topLeft.y);
-    slice.lineTo(topRight.x, topRight.y);
-    slice.lineTo(0, 0);
-    slices.push(slice);
-
-    return (
-      <g key={i}>
-        <line x1={faceCoords.x} y1={faceCoords.y} x2={insetCoords.x} y2={insetCoords.y} />
-        {props.showLabels &&
-          <text x={textCoords.x} y={textCoords.y}>
-            {cat.label}
-          </text>}
-      </g>
-    );
-  });
-
-  const upperCurveMaskId = `curve-upper-${props.id}`;
-  const lowerCurveMaskId = `curve-lower-${props.id}`;
-  const clockMaskId = `clock-${props.id}`;
-
-  const segments = slices.map((s, i) =>
-    <path
-      key={i}
-      d={s}
-      className={styles.segment}
-      style={{ fill: props.palette[i] }}
-      clipPath={`url(#${upperCurveMaskId})`}
-      mask={`url(#${lowerCurveMaskId})`}
-    />
-  );
+  const axisTextShare = -1 / props.categories.length / 2 + 1 / props.categories.length / 30;
+  const axisTextAngle = 180 + clock.getAngleForShare(axisTextShare) / Math.PI * 180;
+  const axisTextCoords = clock.getCoordsForShare(axisTextShare, midShare);
 
   return (
     <g className={props.className}>
-      <defs>
-        <clipPath id={clockMaskId}>
-          <circle r={props.radius} cx="0" cy="0" opacity="0.5" />
-        </clipPath>
-        <clipPath id={upperCurveMaskId}>
-          <path d={upperCurvePath} className={styles.curve} />
-        </clipPath>
-        <mask id={lowerCurveMaskId}>
-          <circle r={props.radius} style={{ fill: 'white' }} />
-          <path d={lowerCurvePath} style={{ fill: 'black' }} />
-        </mask>
-      </defs>
+      <defs />
       <circle className={styles.outer} r={props.radius} cx="0" cy="0" />
-      <g className={styles.ticks}>
-        {tickMarks}
-        {segments}
-        <path d={upperCurvePath} className={styles.curve} />
-        <path
-          d={lowerCurvePath}
-          className={styles.separator}
-          style={{ fill: 'none', stroke: 'black', strokeWidth: 1 }}
-        />
-        {dataPoints}
-      </g>
+      <g className={styles.ticks} />
+      {elems}
       <circle r={midRadius} cx="0" cy="0" className={styles.midRadius} />
       <text
         x={axisTextCoords.x}
