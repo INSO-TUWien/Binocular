ebfc6e75538f0805cdd838dc7021fc8cb06c0435
Alexander Nemetz-Fiedler <alexander.nemetz-fiedler@outlook.com>
2019-08-26 10:51:24 +0200

    :lipstick: Removed COR, added and styled barebones layout for graphs

--- ui/src/visualizations/dashboard/chart/chart.js
+++ ui/src/visualizations/dashboard/chart/chart.js
@@ -105,203 +105,35 @@ export default class CodeOwnershipRiver extends React.Component {
   }
 
   render() {
-    if (!this.props.commits) {
-      return <svg />;
-    }
 
-    const legend = [
-      {
-        name: 'Commits by author',
-        subLegend: this.state.commitLegend
-      }
-    ];
-
-    if (this.props.issues.length > 0) {
-      legend.push({
-        name: 'Issues by state',
-        subLegend: [openIssuesLegend, closedIssuesLegend]
-      });
-    }
-
-    if (this.props.builds.length > 0) {
-      legend.push({
-        name: 'Builds by state',
-        subLegend: [successfulBuildsLegend, unsuccessfulBuildsLegend]
-      });
-    }
-
-    const x = this.scales.scaledX;
-    const y = this.scales.scaledY;
-    const dims = this.state.dimensions;
-    const today = x(new Date());
-    this.scales.x.rangeRound([0, dims.width]);
-    this.scales.y.rangeRound([dims.height, 0]);
-
-    const commitMarkers = this.props.highlightedCommits.map((c, i) => {
-      // for each commit marker, we need to recalculate the correct
-      // y-coordinate by checking where that commit would go in our
-      // commit data points
-      const j = _.sortedIndexBy(this.props.commits, c, other => other.date.getTime());
-
-      const cBefore = this.props.commits[j - 1];
-      const cAfter = this.props.commits[j];
-      const span = cAfter.date.getTime() - cBefore.date.getTime();
-      const dist = c.date.getTime() - cBefore.date.getTime();
-      const pct = dist / span;
-
-      const countDiff = cAfter.totals.count - cBefore.totals.count;
-
-      return (
-        <CommitMarker
-          key={i}
-          commit={c}
-          x={x(c.date)}
-          y={y(cBefore.totals.count + countDiff * pct)}
-          onClick={() => this.props.onCommitClick(c)}
-        />
-      );
-    });
 
     return (
-      <ZoomableChartContainer
-        scaleExtent={[1, Infinity]}
-        onZoom={evt => {
-          this.onZoom(evt);
-          this.props.onViewportChanged(this.scales.scaledX.domain());
-        }}
-        onResize={dims => this.onResize(dims)}
-        onStart={e =>
-          this.setState({
-            isPanning: e.sourceEvent == null || d3.event.sourceEvent.type !== 'wheel'
-          })}
-        onEnd={() => this.setState({ isPanning: false })}
-        className={cx(styles.chart, { [styles.panning]: this.state.isPanning })}>
-        <g>
-          <defs>
-            <clipPath id="chart">
-              <rect x="0" y="0" width={dims.width} height={dims.height} />
-            </clipPath>
-            <clipPath id="x-only">
-              <rect x="0" y={-dims.hMargin} width={dims.width} height={dims.fullHeight} />
-            </clipPath>
-          </defs>
-          <OffsetGroup dims={dims}>
-            <GridLines orient="left" scale={y} ticks="10" length={dims.width} />
-            <GridLines orient="bottom" scale={x} y={dims.height} length={dims.height} />
-            <g>
-              <Axis orient="left" ticks="10" scale={y} />
-              <text x={-dims.height / 2} y={-50} textAnchor="middle" transform="rotate(-90)">
-                Amount
-              </text>
-            </g>
-            <g>
-              <Axis orient="bottom" scale={x} y={dims.height} />
-              <text x={dims.width / 2} y={dims.height + 50} textAnchor="middle">
-                Time
-              </text>
-            </g>
-            <g clipPath="url(#chart)" className={cx(styles.commitCount)}>
-              <StackedArea
-                data={this.props.commits}
-                series={this.state.commitSeries}
-                x={x}
-                y={y}
-                extractX={dateExtractor}
-                sum={_.sum}
-                fillToRight={today}
-              />
-              {commitMarkers}
-            </g>
-            {this.props.highlightedIssue &&
-              <defs>
-                <mask id="issue-mask">
-                  <rect
-                    x={0}
-                    y={0}
-                    width={dims.width}
-                    height={dims.height}
-                    style={{ stroke: 'none', fill: '#ffffff', opacity: 0.5 }}
-                  />
-                  <rect
-                    x={x(this.props.highlightedIssue.createdAt)}
-                    y={0}
-                    width={Math.max(
-                      3,
-                      x(this.props.highlightedIssue.closedAt || new Date()) -
-                        x(this.props.highlightedIssue.createdAt)
-                    )}
-                    height={dims.height}
-                    style={{ stroke: 'none', fill: '#ffffff' }}
-                  />
-                </mask>
-              </defs>}
-            <g
-              clipPath="url(#chart)"
-              mask="url(#issue-mask)"
-              className={cx(styles.openIssuesCount)}>
-              <StackedArea
-                data={this.props.issues}
-                x={x}
-                y={y}
-                series={[
-                  {
-                    extractY: i => i.closedCount,
-                    style: closedIssuesLegend.style,
-                    className: styles.closedIssuesCount,
-                    onMouseEnter: () => this.activateLegend(closedIssuesLegend),
-                    onMouseLeave: () => this.activateLegend(null)
-                  },
-                  {
-                    extractY: i => i.openCount,
-                    style: openIssuesLegend.style,
-                    onMouseEnter: () => this.activateLegend(openIssuesLegend),
-                    onMouseLeave: () => this.activateLegend(null)
-                  }
-                ]}
-                extractX={dateExtractor}
-                sum={_.sum}
-                fillToRight={today}
-              />
-            </g>
-            <g clipPath="url(#chart)" mask="url(#issue-mask)">
-              <StackedArea
-                data={this.props.builds}
-                x={x}
-                y={y}
-                series={[
-                  {
-                    extractY: b => b.stats.success,
-                    style: successfulBuildsLegend.style,
-                    className: '',
-                    onMouseEnter: () => this.activateLegend(successfulBuildsLegend),
-                    onMouseLeave: () => this.activateLegend(null)
-                  },
-                  {
-                    extractY: b => b.stats.failed,
-                    style: unsuccessfulBuildsLegend.style,
-                    onMouseEnter: () => this.activateLegend(unsuccessfulBuildsLegend),
-                    onMouseLeave: () => this.activateLegend(null)
-                  }
-                ]}
-                extractX={dateExtractor}
-                sum={_.sum}
-                fillToRight={today}
-              />
-            </g>
-            <g className={styles.today} clipPath="url(#x-only)">
-              <text x={today} y={-10}>
-                Now
-              </text>
-              <line x1={today} y1={0} x2={today} y2={dims.height} />
-            </g>
-          </OffsetGroup>
-          <Legend
-            x="10"
-            y="10"
-            categories={this.state.hoverHint ? [this.state.hoverHint] : legend}
-          />
-        </g>
-      </ZoomableChartContainer>
+      <div className={styles.chartContainer}>
+        <div className={styles.chartLine}>
+          <div className={styles.text}>
+            CI System
+          </div>
+          <div className={styles.chart}>
+
+          </div>
+        </div>
+        <div className={styles.chartLine}>
+          <div className={styles.text}>
+            Issues
+          </div>
+          <div className={styles.chart}>
+
+          </div>
+        </div>
+        <div className={styles.chartLine}>
+          <div className={styles.text}>
+            Changes
+          </div>
+          <div className={styles.chart}>
+
+          </div>
+        </div>
+      </div>
     );
   }
 
