5888f8e72b1bd14dd603c1d808188ded48277e73
Alexander Nemetz-Fiedler <alexander.nemetz-fiedler@outlook.com>
2019-08-29 16:13:37 +0200

    :construction: Changed Help dialog

--- ui/src/visualizations/dashboard/help.js
+++ ui/src/visualizations/dashboard/help.js
@@ -6,56 +6,31 @@ import styles from './styles.scss';
 
 export default () =>
   <div className={cx('box', styles.help)}>
-    <h1 className="title">Code Ownership River Help</h1>
+    <h1 className="title">Dashboard Help</h1>
     <p>
-      This chart shows the amount of commits created by each developer. Each developer is assigned a
-      color and the respective commit counts are stacked cumulatively on the vertical axis.
+      You can see multiple charts, showing CI (Continuous Integration) builds (split by succeeded/failed),
+      issues (split by opened/closed) and Commit Changes (split by developer) over time.
     </p>
 
     <h2>Interaction</h2>
     <ul>
       <li>
-        <i className="fa fa-arrows-v" /> Use the mouse wheel to zoom into the visualization
-      </li>
-      <li>
-        <i className="fa fa-hand-grab-o" /> Drag the mouse to pan around the chart when zoomed in
-      </li>
-      <li>
-        <i className="fa fa-keyboard-o" /> Press the "0" or "=" keys to reset the viewport
+        <i className="fa fa-hand-grab-o" /> Click and drag on a diagram to zoom in. Double-click to reset.
       </li>
     </ul>
-
-    <p>
-      The legend at the top left corner will show whatever content is currently under the mouse
-      pointer.
-    </p>
 
     <h2>Sidebar</h2>
-    <p>Use the sidebar to switch toggle the categorization of commits.</p>
+    <p>Use the sidebar to switch through chart resolutions. The resolution changes how data is aggregated.</p>
+    <p>Tick "Show developers in CI Builds" to not only show when runs succeded/failed, but who caused them to suceed/fail.</p>
+    <p>Filter issues by open or closed. Open will only show issues that are open today, closed will do the same with closed issues.</p>
+    <p>Use the legend in changes to see who is who, and untick developers to hide them from the graphs. Use the buttons on the bototm
+    to select or deselect all developers.</p>
+    <p>You can swap the change measurement between number of lines changed and number of commits.</p>
     <ul>
-      <li>When "Commits" is selected, commits are graphed according to their absolute counts</li>
+      <li>When "# commits" is selected, commits are graphed according to their absolute counts</li>
       <li>
-        When "Changes" is selected, commits are graphed according to the relative amount of changes
-        (i.e., number of lines changed) by each author. The total height of the commit graph is
-        still measured by the absolute commit count however, in order to keep consistent with the
-        other overlays.
-      </li>
-    </ul>
-
-    <h3>Overlays</h3>
-    <p>Use the sidebar to toggle additional overlays:</p>
-    <ul>
-      <li>
-        <i className="fa fa-ticket" /> Issue overlay: Shows the open and closed issue count on top
-        of the commits. Additionally, a single issue can be highlighted from the sidebar, which will
-        cause the issue's lifetime (e.g. creation until close) in the chart. Additionally, when an
-        issue is highlighted, all commits associated with that issue are shown as green circles on
-        the commit graph. Hover over them to get more info and click them to get to the respective
-        commit in your ITS.
-      </li>
-      <li>
-        <i className="fa fa-server" /> Builds overlay: Shows the amount of successful and
-        unsuccessful builds on top of the commits.
+        When "# lines changed" is selected, commits are graphed according to the relative amount of changes
+        (i.e., number of lines changed) by each author.
       </li>
     </ul>
   </div>;
