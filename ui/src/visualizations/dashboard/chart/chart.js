20e54d23752a92af9d40c462e9c1aad7adf8ea20
Alexander Nemetz-Fiedler <alexander.nemetz-fiedler@outlook.com>
2019-09-02 15:10:10 +0200

    :recycle: Split data extraction of issues and CI into seperate methods from extractCommitData

--- ui/src/visualizations/dashboard/chart/chart.js
+++ ui/src/visualizations/dashboard/chart/chart.js
@@ -13,7 +13,9 @@ export default class Dashboard extends React.Component {
   constructor(props) {
     super(props);
 
-    const { commitSeries, commitLegend, commitChartData, issueChartData, ciChartData, commitScale, issueScale, ciScale } = this.extractCommitData(props);
+    const { commitSeries, commitLegend, commitChartData, commitScale} = this.extractCommitData(props);
+    const {issueChartData, issueScale} = this.extractIssueData(props);
+    const {ciChartData, ciScale} = this.extractCIData(props);
 
     this.state = {
       commitLegend,
@@ -32,7 +34,9 @@ export default class Dashboard extends React.Component {
    * @param nextProps props that are passed
    */
   componentWillReceiveProps(nextProps) {
-    const { commitSeries, commitLegend, commitChartData, issueChartData, ciChartData, commitScale, issueScale, ciScale } = this.extractCommitData(nextProps);
+    const { commitSeries, commitLegend, commitChartData, commitScale} = this.extractCommitData(nextProps);
+    const {issueChartData, issueScale} = this.extractIssueData(nextProps);
+    const {ciChartData, ciScale} = this.extractCIData(nextProps);
     this.setState(
       {
         commitSeries,
@@ -96,6 +100,40 @@ export default class Dashboard extends React.Component {
     );
   }
 
+  extractIssueData(props){
+    if(!props.issues || props.issues.length === 0)
+      return {};
+
+    const issueChartData = [];
+    const issueScale = [0,0];
+    _.each(props.issues, function(issue){
+      issueChartData.push({date: issue.date, openCount: issue.openCount, closedCount: (issue.closedCount > 0) ? (issue.closedCount*(-1)) : 0});
+      if(issueScale[1] < issue.openCount)
+        issueScale[1] = issue.openCount;
+      if(issueScale[0] > issue.closedCount*(-1))
+        issueScale[0] = issue.closedCount*(-1);
+    });
+
+    return {issueChartData, issueScale};
+  }
+
+  extractCIData(props) {
+    if(!props.builds || props.builds.length === 0)
+      return {};
+
+    const ciChartData = [];
+    const ciScale = [0,0];
+    _.each(props.builds, function(build){
+      ciChartData.push({date: build.date, succeeded: build.succeeded, failed: (build.failed > 0) ? (build.failed*(-1)) : 0});
+      if(ciScale[1] < build.succeeded)
+        ciScale[1] = build.succeeded;
+      if(ciScale[0] > build.failed*(-1))
+        ciScale[0] = build.failed*(-1);
+    });
+
+    return {ciChartData, ciScale};
+  }
+
   extractCommitData(props) {
     if (!props.commits || props.commits.length === 0) {
       return {};
@@ -145,26 +183,6 @@ export default class Dashboard extends React.Component {
     });
     //Output in commitChartData has format [{author1: 123, author2: 123, ...}, ...], e.g. series names are the authors with their corresponding values
 
-    const issueChartData = [];
-    const issueScale = [0,0];
-    _.each(props.issues, function(issue){
-      issueChartData.push({date: issue.date, openCount: issue.openCount, closedCount: (issue.closedCount > 0) ? (issue.closedCount*(-1)) : 0});
-      if(issueScale[1] < issue.openCount)
-        issueScale[1] = issue.openCount;
-      if(issueScale[0] > issue.closedCount*(-1))
-        issueScale[0] = issue.closedCount*(-1);
-    });
-
-    const ciChartData = [];
-    const ciScale = [0,0];
-    _.each(props.builds, function(build){
-      ciChartData.push({date: build.date, succeeded: build.succeeded, failed: (build.failed > 0) ? (build.failed*(-1)) : 0});
-      if(ciScale[1] < build.succeeded)
-        ciScale[1] = build.succeeded;
-      if(ciScale[0] > build.failed*(-1))
-        ciScale[0] = build.failed*(-1);
-    });
-
-    return { commitSeries, commitLegend, commitChartData, issueChartData, ciChartData, commitScale: [commitScale/-2, commitScale/2], issueScale: issueScale, ciScale: ciScale };
+    return { commitSeries, commitLegend, commitChartData, commitScale: [commitScale/-2, commitScale/2]};
   }
 }
