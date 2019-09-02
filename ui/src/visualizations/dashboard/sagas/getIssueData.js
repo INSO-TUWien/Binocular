21bfe3d077197b985b7d8439e0524b77104e5d7b
Alexander Nemetz-Fiedler <alexander.nemetz-fiedler@outlook.com>
2019-09-02 11:11:25 +0200

    :construction: Simplified (and made more debuggable) the get issue data algorithm

--- ui/src/visualizations/dashboard/sagas/getIssueData.js
+++ ui/src/visualizations/dashboard/sagas/getIssueData.js
@@ -5,60 +5,45 @@ import _ from 'lodash';
 import { traversePages, graphQl } from '../../../utils';
 
 export default function getIssueData(issueSpan, significantSpan, granularity, interval) {
-  // holds close dates of still open issues, kept sorted at all times
-  const pendingCloses = [];
-
-  // issues closed so far
-  let closeCountTotal = 0,
-    count = 0;
-
-  let next = moment(significantSpan[0]).startOf('day').toDate().getTime();
-  const data = [
-    {
-      date: new Date(issueSpan[0]),
-      count: 0,
-      openCount: 0,
-      closedCount: 0
-    }
-  ];
+  let data = [];
+  let issueList = [];
 
   return traversePages(getIssuesPage(significantSpan[1]), issue => {
-    const createdAt = Date.parse(issue.createdAt);
-    const closedAt = issue.closedAt ? Date.parse(issue.closedAt) : null;
+    issueList.push(issue);
+  }).then(function() {
+    let curr = moment(significantSpan[0]).startOf(granularity.unit).toDate().getTime();
+    let next = curr + interval;
 
-    count++;
+    let sortedCloseDates = [];
+    let createdDate = Date.parse(issueList[0].createdAt);
+    for(let i=0, j=0; curr < significantSpan[1]; curr = next, next += interval){                   //Iterate through time buckets
+      let obj = {date: curr, count: 0, openCount: 0, closedCount: 0};                            //Save date of time bucket, create object
 
-    // the number of closed issues at the issue's creation time, since
-    // the last time we increased closedCountTotal
-    let closedCount = _.sortedIndex(pendingCloses, createdAt);
-    closeCountTotal += closedCount;
-
-    // remove all issues that are closed by now from the "pending" list
-    pendingCloses.splice(0, closedCount);
-
-    while (createdAt >= next) {
-      const dataPoint = {
-        date: new Date(next),
-        count,
-        closedCount: closeCountTotal,
-        openCount: count - closeCountTotal
-      };
-
-      data.push(dataPoint);
-      next += interval;
+      while(i < issueList.length && createdDate < next && createdDate >= curr){               //Iterate through issues that fall into this time bucket
+        if(createdDate > curr && createdDate < next){
+          obj.count++;
+          obj.openCount++;
+        }
+        if(issueList[i].closedAt) {
+          const closedDate = Date.parse(issueList[i].closedAt);
+          const insertPos = _.sortedIndex(sortedCloseDates, closedDate);
+          sortedCloseDates.splice(insertPos, 0, closedDate);
+        }
+        if(++i < issueList.length)
+          createdDate = Date.parse(issueList[i].createdAt);
+      }
+      for(;j < sortedCloseDates.length && sortedCloseDates[j] < next && sortedCloseDates[j] >= curr; j++){               //Iterate through issues that fall into this time bucket
+        if(sortedCloseDates[j] > curr && sortedCloseDates[j] < next){
+          sortedCloseDates.splice(j,1);
+          obj.count++;
+          obj.closedCount++;
+        }
+      }
+      data.push(obj);
     }
 
-    if (closedAt) {
-      // issue has a close date, be sure to track it in the "pending" list
-      const insertPos = _.sortedIndex(pendingCloses, closedAt);
-      pendingCloses.splice(insertPos, 0, closedAt);
-    } else {
-      // the issue has not yet been closed, indicate that by pushing
-      // null to the end of the pendingCloses list, which will always
-      // stay there
-      pendingCloses.push(null);
-    }
-  }).then(() => data);
+    return data;
+  });
 }
 
 const getIssuesPage = until => (page, perPage) => {
