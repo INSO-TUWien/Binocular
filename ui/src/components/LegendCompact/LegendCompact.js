8192ab1aa2f2ab614353de843f233e9e2b50728d
Alexander Nemetz-Fiedler <alexander.nemetz-fiedler@outlook.com>
2019-09-09 14:09:58 +0200

    :construction: Changed LegendCompact so that it accepts either one or two colors

--- ui/src/components/LegendCompact/LegendCompact.js
+++ ui/src/components/LegendCompact/LegendCompact.js
@@ -15,10 +15,17 @@ export default class LegendCompact extends React.Component {
   }
 
   render() {
+    let rects = [];
+    if(this.props.color2) {
+      rects.push(<rect width={ICON_WIDTH/2} height={ICON_HEIGHT} fill={this.props.color} key={this.props.color}/>);
+      rects.push(<rect width={ICON_WIDTH/2} height={ICON_HEIGHT} x={ICON_WIDTH/2} fill={this.props.color2} key={this.props.color2}/>);
+    }else{
+      rects.push(<rect width={ICON_WIDTH} height={ICON_HEIGHT} fill={this.props.color} key={this.props.color}/>);
+    }
+
     return (<div className={styles.legend}>
       <svg className={styles.icon} width={ICON_WIDTH} height={ICON_HEIGHT}>
-        <rect width={ICON_WIDTH/2} height={ICON_HEIGHT} fill={this.props.color}/>
-        <rect width={ICON_WIDTH/2} height={ICON_HEIGHT} x={ICON_WIDTH/2} fill={this.props.color2}/>
+        {rects}
       </svg>
       <text>
         {this.props.text}
