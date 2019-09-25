dd44c16fd937831e2f9e2e439fd614438d0f1c04
Amra Goro <e1626571@student.tuwien.ac.at>
2019-09-25 15:43:45 +0200

    safe state 15

--- ui/src/visualizations/code-ownership-transfer/sagas/getOwner.js
+++ ui/src/visualizations/code-ownership-transfer/sagas/getOwner.js
@@ -9,45 +9,59 @@ export default function getOwnershipList(selectedFile) {
     for (let k = 0; k < selectedFile.commits[i].hunks.length; k++) {
       //Just adding new lines
       if (selectedFile.commits[i].hunks[k].newLines > 0 && selectedFile.commits[i].hunks[k].oldLines === 0) {
-        for (let j = selectedFile.commits[i].hunks[k].newStart - 1; j <  (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].newLines) ; j++) {
-          ownersPerLine.splice(j, 0, selectedFile.commits[i].signature);
+        let tempArray = [...ownersPerLine];
+        for (let j = selectedFile.commits[i].hunks[k].newStart - 1; j < (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].newLines); j++) {
+          tempArray.splice(j, 0, selectedFile.commits[i].signature);
         }
-        console.log('ADD LINES', ownersPerLine);
+        ownersPerLine = [...tempArray];
+        console.log('ADD LINES after', ownersPerLine);
       }
       //Just deleting lines
 
       else if (selectedFile.commits[i].hunks[k].newLines === 0 && selectedFile.commits[i].hunks[k].oldLines > 0) {
+        let tempArray = [...ownersPerLine];
         for (let j = selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].oldLines; j >= selectedFile.commits[i].hunks[k].newStart; j--) {
-          ownersPerLine.splice(j, 1);
+          tempArray.splice(j, 1);
         }
+        ownersPerLine = [...tempArray];
         console.log('DELETE LINES', ownersPerLine);
       }
-      // //Overwriting lines
-      // else {
-      //   if (selectedFile.commits[i].hunks[k].newLines === selectedFile.commits[i].hunks[k].oldLines) {
-      //     for (let j = selectedFile.commits[i].hunks[k].newStart; j < (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].newLines); j++) {
-      //       //just overwriting
-      //       ownersPerLine[j] = selectedFile.commits[i].signature;
-      //     }
-      //   } else if (selectedFile.commits[i].hunks[k].newLines > selectedFile.commits[i].hunks[k].oldLines) {
-      //     //overwriting + adding
-      //     for (let j = selectedFile.commits[i].hunks[k].newStart; j < (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].oldLines); j++) {
-      //       ownersPerLine[j] = selectedFile.commits[i].signature;
-      //     }
-      //     for (let j = (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].oldLines); j < (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].newLines); j++) {
-      //       ownersPerLine.splice(j, 0, selectedFile.commits[i].signature);
-      //     }
-      //   } else if(selectedFile.commits[i].hunks[k].newLines < selectedFile.commits[i].hunks[k].oldLines) {
-      //     //overwriting + deleting
-      //     for (let j = selectedFile.commits[i].hunks[k].newStart; j < (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].newLines); j++) {
-      //       ownersPerLine[j] = selectedFile.commits[i].signature;
-      //     }
-      //     for (let j = (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].newLines); j < (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].oldLines); j++) {
-      //       ownersPerLine.splice(j, 1);
-      //     }
-      //   }
-      //   console.log('Overwriting', ownersPerLine);
-      // }
+      //Overwriting lines
+
+      else {
+        if (selectedFile.commits[i].hunks[k].newLines === selectedFile.commits[i].hunks[k].oldLines) {
+          //just overwriting
+          let tempArray = [...ownersPerLine];
+          for (let j = selectedFile.commits[i].hunks[k].newStart - 1; j < (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].newLines); j++) {
+            tempArray[j] = selectedFile.commits[i].signature;
+          }
+          ownersPerLine = [...tempArray];
+          console.log('just Overwriting', ownersPerLine);
+        } else if (selectedFile.commits[i].hunks[k].newLines > selectedFile.commits[i].hunks[k].oldLines) {
+          //overwriting + adding
+          let tempArray = [...ownersPerLine];
+          for (let j = selectedFile.commits[i].hunks[k].newStart - 1; j < (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].oldLines); j++) {
+            tempArray[j] = selectedFile.commits[i].signature;
+          }
+          //adding
+          for (let j = (selectedFile.commits[i].hunks[k].newStart + selectedFile.commits[i].hunks[k].oldLines - 1); j < (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].newLines); j++) {
+            tempArray.splice(j, 0, selectedFile.commits[i].signature);
+          }
+          ownersPerLine = [...tempArray];
+          console.log('Overwriting + adding ', ownersPerLine);
+        } else if (selectedFile.commits[i].hunks[k].newLines < selectedFile.commits[i].hunks[k].oldLines) {
+          //overwriting + deleting
+          let tempArray = [...ownersPerLine];
+          for (let j = selectedFile.commits[i].hunks[k].newStart - 1; j < (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].newLines); j++) {
+            tempArray[j] = selectedFile.commits[i].signature;
+          }
+          for (let j = (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].oldLines); j > (selectedFile.commits[i].hunks[k].newStart - 1 + selectedFile.commits[i].hunks[k].newLines) ; j--) {
+            tempArray.splice(j, 1);
+          }
+          ownersPerLine = [...tempArray];
+          console.log('Overwriting + deleting ', ownersPerLine);
+        }
+      }
     }
     ownershipOfFileList.push(ownersPerLine);
   }
