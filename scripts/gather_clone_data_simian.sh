eff9a6df544a0d65c4646c3173c74049eba07fa9
barca899 <thomas.burghard@gmail.com>
2019-12-08 13:04:07 +0100

    minor adaptations to gather clone date
    Issue#12

--- scripts/gather_clone_data_simian.sh
+++ scripts/gather_clone_data_simian.sh
@@ -22,37 +22,37 @@ cd $DIRECTORY/../$PROJECT_NAME
 mkdir -p $CLONEDIR
 
 echo Getting Revisions
-#git rev-list HEAD --reverse > $REVS
+git rev-list HEAD --reverse > $REVS
 
 # remove already indexed revisions from clone detection
-#if [[ "${LASTREV}" != "UNDEFINED" ]];
-#then
-#    echo Removing already indexed Revisions
-#    sed  -i "0,/^${LASTREV}$/d" ${REVS}
-#fi
+if [[ "${LASTREV}" != "UNDEFINED" ]];
+then
+    echo Removing already indexed Revisions
+    sed  -i "0,/^${LASTREV}$/d" ${REVS}
+fi
 
 echo Starting clone detection
 cat $REVS | while read line
 do
     echo Current revision: $line
-    #git checkout $line
+    git checkout $line
 
     echo Detecting type 1 clones
-    #java -jar ${CLONEDETECTOR} . -threshold=4 -language=java -includes=**/*.java -formatter=xml -reportDuplicateText > $CLONEDIR/simian_${line}_type1.xml    
+    java -jar ${CLONEDETECTOR} . -threshold=4 -language=java -includes=**/*.java -formatter=xml -reportDuplicateText > $CLONEDIR/simian_${line}_type1.xml    
 
     echo Detecting type 2 clones
-    #java -jar ${CLONEDETECTOR} . -threshold=4 -language=java -includes=**/*.java -formatter=xml -reportDuplicateText -ignoreIdentifiers -ignoreLiterals -ignoreVariableNames > $CLONEDIR/simian_${line}_type2.xml
+    java -jar ${CLONEDETECTOR} . -threshold=4 -language=java -includes=**/*.java -formatter=xml -reportDuplicateText -ignoreIdentifiers -ignoreLiterals -ignoreVariableNames > $CLONEDIR/simian_${line}_type2.xml
 
     break;
 done
 
 cd $CLONEDIR
 # remove all files that contain no clones
-#grep -rL '<block sourceFile' *.xml | xargs rm -f
+grep -rL '<block sourceFile' *.xml | xargs rm -f
 
 # remove first 3 lines of Simian report as it is just license info
-#sed -i '1,3d' *.xml
+sed -i '1,3d' *.xml
 
 echo Finished clone detection
 
-sleep 5s
+sleep 1s
