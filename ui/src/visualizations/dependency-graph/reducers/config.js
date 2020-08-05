e8e57792f99673a40ce52cefde2abe08e80ecd8a
Manuel Bacher <manuel.bacher@syn-group.com>
2020-08-05 15:52:16 +0200

    don't reload fileTree when changing depth filter

--- ui/src/visualizations/dependency-graph/reducers/config.js
+++ ui/src/visualizations/dependency-graph/reducers/config.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 
 export default handleActions(
   {
-    SET_DEPTH: (state, action) => _.assign({}, state, { depth: parseInt(action.payload, 10), fileTree: [], reloaded: true }),
+    SET_DEPTH: (state, action) => _.assign({}, state, { depth: parseInt(action.payload, 10), reloaded: true }),
     SET_COMBINED_THRESHHOLD: (state, action) => _.assign({}, state, { meanPercentageOfCombinedCommitsThreshold: parseInt(action.payload, 10), reloaded: false }),
     SET_MAX_THRESHHOLD: (state, action) => _.assign({}, state, { meanPercentageOfMaxCommitsThreshold: parseInt(action.payload, 10), reloaded: false }),
     SET_FILES: (state, action) => _.assign({}, state, { fileTree: action.payload, reloaded: false }),
