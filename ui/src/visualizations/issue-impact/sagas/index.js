02a83b3cd421bb676d043d6b546f56a82c105fd2
THMv1TU <e1226762@student.tuwien.ac.at>
2020-08-11 23:56:32 +0200

    Language and Module Visualization #10 +) linting issues +) improve .gitignore

--- ui/src/visualizations/issue-impact/sagas/index.js
+++ ui/src/visualizations/issue-impact/sagas/index.js
@@ -86,6 +86,9 @@ export const fetchIssueImpactData = fetchFactory(
                        oldLines
                        webUrl
                      }
+                     stats {
+                      
+                     }
                      file {
                        id
                        path
