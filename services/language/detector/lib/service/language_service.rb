2be0021bf01e9c151ec81c49d8e50cc493478174
THMv1TU <e1226762@student.tuwien.ac.at>
2020-08-16 12:10:05 +0200

    Language and Module Visualization #10 +) improve language handling +) language detection refactor ~) linguist has some performance issues for some particular files grpc timeout required

--- services/language/detector/lib/service/language_service.rb
+++ services/language/detector/lib/service/language_service.rb
@@ -42,11 +42,28 @@ module Binocular
         if @register.token == request.token
           begin
             @logger.info("Processing #{request.path}")
+            generated = Linguist::Generated.generated?(request.path, request.content)
+
+            if generated
+              @logger.info("#{request.path} is a generated file!")
+              return nil
+            end
+
+            # file is to small to detect
+            if request.content.length < 15
+              @logger.info("#{request.path} is too small!")
+              return nil
+            end
+
             fileBlob = Linguist::Blob.new(request.path, request.content)
             language = Linguist.detect(fileBlob)
             if language.nil?
               return nil
             end
+
+            detect_event = Linguist.instrumenter.events.last
+            detect_event_payload = detect_event[:args].first
+
             @logger.info("#{request.path} processed successfully")
             if language.group.nil?
               return Binocular::Comm::Language.new(
