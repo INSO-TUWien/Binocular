9f248eccce59c5a48e40bbfb39180f62bbb74f5e
Johann Grabner <johann.grabner@inso.tuwien.ac.at>
2021-05-03 19:15:58 +0200

    :rotating_light: #10: Fix most RuboCop warnings

--- services/language/detector/lib/service/language_service.rb
+++ services/language/detector/lib/service/language_service.rb
@@ -1,35 +1,33 @@
-
 require 'grpc'
 require 'api/language.service_services_pb'
 require 'service/i_service'
-
 
 module Binocular
   module Service
     class LanguageService < Binocular::Comm::LanguageDetectionService::Service
       include Binocular::Service::ILanguageService
-      
+
       # initialises all needed data
       # @param [Binocular::Config] config contains the merged config file data
       # @param [Binocular::Service::IRegistrationService] register contains the gateway communication service
       # @param [GRPC::RpcServer] rpc_service contains the grpc server
       def initialize(config, register, rpc_service)
         @register = register
-        @rpc_service  = rpc_service
+        @rpc_service = rpc_service
         @config = config
 
-        @server_address = "0.0.0.0:#{config.data.dig('languageService','port')}"
+        @server_address = "0.0.0.0:#{config.data.dig('languageService', 'port')}"
         @rpc_service.add_http2_port(@server_address, :this_port_is_insecure)
         @rpc_service.handle(self)
-        @logger = Logger.new(config.data.dig('languageService','logger', 'file'))
-        @logger.level = Logger::Severity.const_get(config.data.dig('languageService','logger', 'level'))
+        @logger = Logger.new(config.data.dig('languageService', 'logger', 'file'))
+        @logger.level = Logger::Severity.const_get(config.data.dig('languageService', 'logger', 'level'))
       end
 
       # start the language detection service to listen for grpc requests
       def start
         @logger.info("Start language service listening on #{@server_address}")
         @rpc_service.run_till_terminated_or_interrupted(['INT', 'TERM'])
-        @logger.info("Stopped language service...")
+        @logger.info('Stopped language service...')
       end
 
       def stop
@@ -55,27 +53,30 @@ module Binocular
             # try to get the main category to prevent various subtypes of the same language
             if language.group.nil?
               return Binocular::Comm::Language.new(
-                  :id =>  language.group.language_id,
-              :name => language.group.name,
-              :popular => language.group.popular?,
-              :aliases => language.group.aliases,
-              :color => language.group.color
+                id: language.group.language_id,
+                name: language.group.name,
+                popular: language.group.popular?,
+                aliases: language.group.aliases,
+                color: language.group.color
               )
             end
             return Binocular::Comm::Language.new(
-              :id => language.language_id,
-              :name => language.name,
-              :popular => language.popular?,
-              :aliases => language.aliases,
-              :color => language.color
+              id: language.language_id,
+              name: language.name,
+              popular: language.popular?,
+              aliases: language.aliases,
+              color: language.color
             )
-          rescue
-            error = $!
+          rescue StandardError
+            error = $ERROR_INFO
             @logger.error(error.message)
             return nil
           end
         end
-        raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::INVALID_ARGUMENT, "The provided token does not match with the one provided by the gateway!")
+        raise GRPC::BadStatus.new(
+          GRPC::Core::StatusCodes::INVALID_ARGUMENT,
+          'The provided token does not match with the one provided by the gateway!'
+        )
       end
 
       # disconnects this service if the token is equal
@@ -85,7 +86,10 @@ module Binocular
           @register.disconnect
           return Binocular::Comm::UnregisterResponse.new
         end
-        raise GRPC::BadStatus.new(GRPC::Core::StatusCodes::INVALID_ARGUMENT, "The provided token does not match with the one provided by the gateway!")
+        raise GRPC::BadStatus.new(
+          GRPC::Core::StatusCodes::INVALID_ARGUMENT,
+          'The provided token does not match with the one provided by the gateway!'
+        )
       end
     end
   end
