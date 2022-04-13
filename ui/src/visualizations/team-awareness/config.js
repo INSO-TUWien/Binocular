86c6c541a86e630b97fcaa45fbc2451f0bf29721
Michael Kurz <michael.kurz@student.tuwien.ac.at>
2022-04-13 21:10:38 +0200

    #38: add styling to config components

--- ui/src/visualizations/team-awareness/config.js
+++ ui/src/visualizations/team-awareness/config.js
@@ -7,7 +7,7 @@ import * as d3 from 'd3';
 import { getState } from './util/util';
 import _ from 'lodash';
 import ActivityTimeline from './components/Timeline/ActivityTimeline';
-import styles from './config.scss';
+import styles from './styles.scss';
 
 const mapStateToProps = (appState /*, ownProps*/) => {
   const { config, data } = getState(appState);
@@ -34,23 +34,30 @@ class ConfigComponent extends React.Component {
   }
 
   render() {
-    const { onActivityDimensionsRestricted, onSelectActivityScale } = this.props;
+    const { onActivityDimensionsRestricted, onSelectActivityScale, config } = this.props;
     const { activityTimeline, yDims } = this.props.data;
     return (
       <div className={styles.configContainer}>
-        <div>
-          <div>Activity:</div>
-          <div>
-            <select value={this.props.config.selectedActivityScale} onChange={value => onSelectActivityScale(value)}>
-              <option value="commits">Commits</option>
-              <option value="activity">Additions & Deletions</option>
-              <option value="additions">Additions</option>
-              <option value="deletions">Deletions</option>
-            </select>
+        <form>
+          <div className="field">
+            <label className="label">Activity</label>
+            <div className="control">
+              <div className={'select ' + styles.activitySelect}>
+                <select
+                  className={styles.activitySelect}
+                  value={config.selectedActivityScale}
+                  onChange={value => onSelectActivityScale(value)}>
+                  <option value="commits">Commits</option>
+                  <option value="activity">Additions & Deletions</option>
+                  <option value="additions">Additions</option>
+                  <option value="deletions">Deletions</option>
+                </select>
+              </div>
+            </div>
           </div>
-        </div>
+        </form>
         <div>
-          <div>Timeline</div>
+          <label className="label">Timeline</label>
           <ActivityTimeline
             palette={{ activity: '#00bcd4' }}
             paddings={{ top: 20, left: 25, bottom: 30, right: 30 }}
