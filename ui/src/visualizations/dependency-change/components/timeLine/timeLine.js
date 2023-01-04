dc2c392be297111e75d4c99b3d19926e321e3ec0
Solaiman Nejat <solaiman.nejat@bat.at>
2023-01-04 15:33:55 +0100

    add tooltip

--- ui/src/visualizations/dependency-change/components/timeLine/timeLine.js
+++ ui/src/visualizations/dependency-change/components/timeLine/timeLine.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import styles from './timeLine.scss';
 import * as d3 from 'd3';
-import cx from 'classnames'
+import cx from 'classnames';
+import { func } from 'prop-types';
 
 
 export default class TimeLineComponent extends React.PureComponent {
@@ -18,7 +19,7 @@ export default class TimeLineComponent extends React.PureComponent {
         let data = [];
 
         for(const commit of this.props.data){
-            data.push({date: new Date(commit.date), value: 0, label: commit.version});
+            data.push({date: new Date(commit.date), value: 0, label: commit.version, committer: commit.committer});
         }
         
         const svg = d3.select(this.g);
@@ -35,6 +36,8 @@ export default class TimeLineComponent extends React.PureComponent {
 
         svg.append("g").attr("transform", "translate(0,100)").call(xAxis);
 
+        const tooltip = d3.select('.tooltip');
+
         svg
         .selectAll("circle")
         .data(data)
@@ -45,6 +48,32 @@ export default class TimeLineComponent extends React.PureComponent {
         .attr("cy", d => yScale(d.value))
         .attr("fill", "blue")
         .attr("title", d => d.label)
+        .on('mouseover', function(e) {
+          const data = d3.select(this).datum();
+          d3.select(".tooltip").style("display","flex");
+          tooltip
+            .style('top', (e.pageY + 10) + 'px')
+            .style('left', e.pageX + 'px');
+
+            const dateOptions = {
+              year: 'numeric',
+              month: '2-digit',
+              day: '2-digit'
+            };
+            
+            const timeOptions = {
+              hour: '2-digit',
+              minute: '2-digit'
+            };
+
+            const formattedDate = data.date.toLocaleDateString('de-DE', dateOptions);
+            const formattedTime = data.date.toLocaleTimeString('de-DE', timeOptions);
+
+          tooltip.html(`<span>${data.committer}</span><span>${formattedDate} ${formattedTime}</span>`);
+        })
+        .on('mouseout', function() {
+          d3.select(".tooltip").style("display", "none");
+        })
         .append("text")
         .text(d => d.label)
         .attr("x", d => xScale(d.date))
@@ -57,6 +86,8 @@ export default class TimeLineComponent extends React.PureComponent {
         .style("glyph-orientation-vertical", 0)
         .style("font-weight", "bold")
         .style("text-anchor", "start");
+
+        
      
         const circles = document.querySelectorAll(".timeLine circle");
         circles.forEach(elem => {
@@ -70,6 +101,7 @@ export default class TimeLineComponent extends React.PureComponent {
     return (
         <div className={cx(styles.chartDiv, 'timeLine')}>
           <svg className={styles.chartSvg} ref={g => (this.g = g)}></svg>
+          <div className={cx(styles.tooltip, 'tooltip')}>Hallo</div>
         </div>
     );
   }
