d158f27eddb09bb61f96a46a32938328d9fcf020
belthazors <amarkasic@hotmail.com>
2023-04-12 02:16:12 +0200

    #96: Command-Line Interface redesign
    * created "adapter" cli.js file with a couple of options added

---
+++ cli.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const { Command } = require('commander');
+const { spawn } = require('child_process');
+const figlet = require('figlet');
+const chalk = require('chalk');
+const package_json = require('./package.json');
+
+const cli = new Command();
+
+// Show help when no arguments passed
+if (process.argv.length < 3) {
+  cli.help();
+}
+
+// Add unknown option handler
+cli.showHelpAfterError();
+
+// Maybe figure out how to securely load the version file from package.json
+cli
+  .version(package_json.version)
+  .description(
+    'Binocular is a tool for visualizing data from various software-engineering tools.' +
+      ' It works as a command-line tool run from a git-repository.' +
+      ' When run, Binocular will gather data from the repository ' +
+      'and the GitLab REST-API and persist it to a configured ArangoDB instance.\n' +
+      '\n' +
+      'Binocular then hosts interactive visualizations about the gathered data via a web-interface.'
+  )
+  .option('-e, --export-db <repo>', 'Index the repository <repo> and export the ArangoDB database')
+  .option('-o, --build-offline <repo>', 'Prepares the artifacts needed for an offline visualization of the Repository <repo>')
+  .parse(process.argv);
+
+console.log(chalk.green(figlet.textSync('Binocular')));
+
+const options = cli.opts();
+
+if (options.exportDb) {
+  console.log(chalk.cyan(`Indexing repository ${options.exportDb} in offline mode and exporting the database...`));
+  execute(`node binocular.js --no-open --no-server ${options.exportDb}`);
+}
+
+if (options.buildOffline) {
+  console.log(chalk.cyan(`Indexing repository ${options.buildOffline} in offline mode and exporting the database...`));
+  execute(`node binocular.js --no-open --no-server ${options.buildOffline}`, 'npm run build');
+}
+
+// Executes statements sequentially
+function execute(...statements) {
+  const chainedStatement = chainStatements(...statements);
+  spawn(chainedStatement, { stdio: 'inherit', shell: true });
+
+  // SIGINT gets sent to the entire process group, so no need to send it again to the child process
+  process.on('SIGINT', () => {});
+
+  function chainStatements(...statements) {
+    return statements.reduce((acc, statement) => {
+      if (acc === '') {
+        return statement;
+      } else {
+        return acc + ' && ' + statement;
+      }
+    }, '');
+  }
+}
