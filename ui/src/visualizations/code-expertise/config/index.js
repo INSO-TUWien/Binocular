eb0a8f41ab92ae570c437b46ae978b24dc3d67cf
Maximilian Zenz <e1633058@student.tuwien.ac.at>
2023-04-17 16:10:42 +0200

    #93: Bluebird outdated
    * resolved some conflicts after merge with current dev branch
    *removing bluebird from db.js

--- ui/src/visualizations/code-expertise/config/index.js
+++ ui/src/visualizations/code-expertise/config/index.js
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Promise from 'bluebird';
 import _ from 'lodash';
 
 import TabCombo from '../../../components/TabCombo.js';
