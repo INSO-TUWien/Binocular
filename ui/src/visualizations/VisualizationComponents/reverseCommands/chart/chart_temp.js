6668d740835feb59599197947a9e26b2bae59e62
Valentin Kastberger <e11905155@student.tuwien.ac.at>
2023-08-14 18:06:25 +0200

    add commit summary to view

--- ui/src/visualizations/VisualizationComponents/reverseCommands/chart/chart_temp.js
+++ ui/src/visualizations/VisualizationComponents/reverseCommands/chart/chart_temp.js
@@ -13,27 +13,14 @@ export default class ReverseCommands extends React.Component {
 
     this.state = {
       branch: 'main',
+      clickedNode: null,
+      clickedCommitInfo: null,
+      popupVisible: false,
     };
   }
 
   render() {
     if (this.props.filteredCommits.length > 0) {
-      /*const graph = {
-        nodes: [
-          { id: 1, label: "Node 1", title: "node 1 tootip text" },
-          { id: 2, label: "Node 2", title: "node 2 tootip text" },
-          { id: 3, label: "Node 3", title: "node 3 tootip text" },
-          { id: 4, label: "Node 4", title: "node 4 tootip text" },
-          { id: 5, label: "Node 5", title: "node 5 tootip text" }
-        ],
-        edges: [
-          { from: 1, to: 2 },
-          { from: 1, to: 3 },
-          { from: 2, to: 4 },
-          { from: 2, to: 5 }
-        ]
-      };*/
-
       const nodes = [];
       const edges = [];
 
@@ -61,6 +48,13 @@ export default class ReverseCommands extends React.Component {
       const events = {
         select: function(event) {
           var { nodes, edges } = event;
+        },
+        click: event => {
+          if (event.nodes.length === 1) {
+            console.log(event);
+            const clickedNodeId = event.nodes[0];
+            this.handleNodeClick(clickedNodeId); // Update the state
+          }
         }
       };
       return (
@@ -74,47 +68,12 @@ export default class ReverseCommands extends React.Component {
                 //  if you want access to vis.js network api you can set the state in a parent component using this property
               }}
             />
+            {this.state.popupVisible && this.state.clickedNode && (
+             this.getCommitInfos()
+            )}
           </div>
         </div>
       );
-
-      /*const g = new graphlib.Graph();
-      const nodes = [];
-      const edges = [];
-
-      this.props.filteredCommits.forEach((commit) => {
-        nodes.push({ id: commit.sha, label: commit.sha });
-        edges.push({ from: commit.parent, to: commit.id });
-      });
-
-      dagre.layout(g);
-
-      const graph = {
-        nodes,
-        edges,
-      };
-
-      const options = {
-        layout: {
-          hierarchical: true,
-        },
-      }; */
-
-      //return <Graph graph={graph} options={options} />;
-      /*const commits = this.props.filteredCommits;
-      if(commits.length > 0) {
-        console.log('println ',commits[0]);
-      } else {
-        console.log(':(');
-      }*/
-
-      const chart = (
-        <div>
-          <Graph graph={graph} options={options} />
-        </div>
-      );
-
-      return chart;
     } else {
       const chart = (
         <div>
@@ -124,27 +83,40 @@ export default class ReverseCommands extends React.Component {
     }
   }
 
-  filterCommits(commits, branchName) {
-    const filtered = commits[0].filter((commit) => commit.branch === branchName);
-    console.log(filtered);
-    return filtered;
+  handleNodeClick = nodeId => {
+    // Update the state to the clicked node ID
+    this.setState({    popupVisible: false});
+    this.setState({ clickedNode: nodeId });
+    this.setCommitInfos(nodeId);
+    this.setState({    popupVisible: true});
+  };
+
+  setCommitInfos( nodeId ) {
+    const commitInfo = this.props.filteredCommits.find((commit) => commit.sha === nodeId);
+    this.setState({ clickedCommitInfo: commitInfo });
+    console.log(this.state.clickedCommitInfo);
   }
 
-  drawNode() {
-    // Implement your circle drawing logic here
-    // You might use SVG <circle> element or other methods
+  getCommitInfos() {
+    const commit = this.state.clickedCommitInfo;
     return (
-      <svg width="100" height="100">
-        <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="2" fill="red" />
-      </svg>
+      <div
+        className="popup"
+        style={{
+          position: 'absolute',
+          top: `${this.state.clickedNode.y}px`,
+          left: `${this.state.clickedNode.x}px`,
+        }}
+      >
+        <div className={styles.commitHeading}>
+          Commit-Summary:
+        </div>
+        <br/>
+        <b>SHA:</b> {commit.sha} <br/>
+        <b>Author:</b> {commit.signature} <br/>
+        <b>Branch:</b> {commit.branch} <br/> <br/>
+        <b>Message:</b> {commit.message} <br/>
+      </div>
     );
   }
-  buildGraph(g, nodes, edges) {
-
-  }
-
-  drawConnection(node,commits) {
-
-  }
-
 }
