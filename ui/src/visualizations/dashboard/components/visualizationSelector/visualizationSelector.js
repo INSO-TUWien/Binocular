2b10adc6d89ab1495857175abe4e37ecbfcc07da
Maximilian Zenz <e1633058@student.tuwien.ac.at>
2023-09-21 15:07:09 +0200

    #160: Visualizations don't load in Dashboard when adding a new one
    - changing visualizationSelector.js to new React syntax

--- ui/src/visualizations/dashboard/components/visualizationSelector/visualizationSelector.js
+++ ui/src/visualizations/dashboard/components/visualizationSelector/visualizationSelector.js
@@ -1,42 +1,35 @@
 'use strict';
 
 import React from 'react';
-import visualizationRegistry from '../../visualizationRegistry';
 import styles from './visualizationSelector.scss';
 
-export default class VisualizationSelector extends React.PureComponent {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className={styles.visualizationSelector}>
-        <div className={styles.backgroundBlur} onClick={this.props.close}>
-          <div
-            className={styles.visualizationContainer}
-            onClick={(event) => {
-              event.stopPropagation();
-            }}>
-            <h1>Select Visualization you want to add!</h1>
-            <hr />
-            {Object.keys(this.props.visualizations).map((viz) => {
-              const visualization = this.props.visualizations[viz];
-              return (
-                <button
-                  key={visualization.id}
-                  className={styles.visualizationSelectButton}
-                  onClick={(e) => {
-                    this.props.addVisualization(viz);
-                    this.props.close();
-                  }}>
-                  {visualization.label}
-                </button>
-              );
-            })}
-          </div>
+export default (props) => {
+  return (
+    <div className={styles.visualizationSelector}>
+      <div className={styles.backgroundBlur} onClick={props.close}>
+        <div
+          className={styles.visualizationContainer}
+          onClick={(event) => {
+            event.stopPropagation();
+          }}>
+          <h1>Select Visualization you want to add!</h1>
+          <hr />
+          {Object.keys(props.visualizations).map((viz) => {
+            const visualization = props.visualizations[viz];
+            return (
+              <button
+                key={visualization.id}
+                className={styles.visualizationSelectButton}
+                onClick={(e) => {
+                  props.addVisualization(viz);
+                  props.close();
+                }}>
+                {visualization.label}
+              </button>
+            );
+          })}
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
