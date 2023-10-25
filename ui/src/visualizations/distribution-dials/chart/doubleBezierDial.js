71584c58bf0a6daf1d1fdcc609c6114a3b1b697b
Nyzabes <96738332+Nyzabes@users.noreply.github.com>
2023-10-25 15:14:58 +0200

    Distribution Dials: removed unnecessary dial
    - removed double bezier dial since it makes the chart too busy, and it can be replaced with the stacked dial
    - #151

--- ui/src/visualizations/distribution-dials/chart/doubleBezierDial.js
+++
@@ -1,8 +0,0 @@
-import * as d3 from 'd3';
-import React, { useState, useEffect } from 'react';
-
-function DoubleBezierDial({ innerRadius, outerRadius, dataOutside, dataInside }) {
-  return <circle cx="0" cy="0" r={outerRadius} stroke="black" fill="white" />;
-}
-
-export default DoubleBezierDial;
