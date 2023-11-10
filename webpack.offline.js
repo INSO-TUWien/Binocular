7acdbbaae8d155ee7e1716e52c432ada9912bf8c
Manuel Stöger <manuel.stoeger@inso.tuwien.ac.at>
2023-11-10 12:55:54 +0100

    #181: Updated offline webpack

--- webpack.offline.js
+++ webpack.offline.js
@@ -1,7 +1,6 @@
-'use strict';
-
 import { merge } from 'webpack-merge';
-import TerserPlugin from "terser-webpack-plugin"
+import TerserPlugin from 'terser-webpack-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import * as commonConfig from './webpack.common.js';
 import path from 'path';
 import { createRequire } from 'node:module';
@@ -16,7 +15,6 @@ const opts = {
 
 export default merge(commonConfig, {
   mode: 'production', // for webpack internal optimization
-  // devtool: 'cheap-module-source-map',
   entry: [require.resolve('babel-polyfill'), './ui/src/index'],
   module: {
     rules: [
@@ -27,13 +25,67 @@ export default merge(commonConfig, {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './ui/index.html',
+      inject: true,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      },
+    }),
+  ],
   optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 30000,
+      maxSize: 0,
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
     minimize: true,
     minimizer: [
       new TerserPlugin({
-        parallel: 4,
+        terserOptions: {
+          parse: {
+            ecma: 2018,
+          },
+          compress: {
+            ecma: 5,
+            comparisons: false,
+            inline: 2,
+          },
+          mangle: {
+            safari10: true,
+          },
+          keep_classnames: false,
+          keep_fnames: false,
+        },
       }),
-    ]
+    ],
+    runtimeChunk: {
+      name: (entrypoint) => `runtime~${entrypoint.name}`,
+    },
+    removeAvailableModules: true,
+    removeEmptyChunks: true,
+    mergeDuplicateChunks: true,
   },
   output: {
     path: path.join(__dirname, '/dist'),
