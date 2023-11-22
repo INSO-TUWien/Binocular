fe83588aaa9fbb17d54940b1510181c4aadbe815
Anton Martinovic <e52004305@student.tuwien.ac.at>
2023-11-22 21:06:15 +0100

    Added paginated getComments method and fixed paginatedRequest to differentiate between the return types of requests for issues and comments. Various different fixes

--- lib/core/provider/jira.ts
+++ lib/core/provider/jira.ts
@@ -1,42 +1,42 @@
-"use strict";
-
-
+'use strict';
 
 import debug from 'debug';
 
 const log = debug('jira');
 
-import Paginator from "../../paginator";
-import urlJoin from "url-join";
-
+import Paginator from '../../paginator';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import urlJoin from 'url-join';
 
 class Jira {
-    private baseUrl: any;
-    private privateToken: any;
-    private requestTimeout: any;
-    private count: number;
-    private stopping: boolean;
+  private baseUrl: any;
+  private privateToken: any;
+  private requestTimeout: any;
+  private count: number;
+  private stopping = false;
   private usermail: any;
-  constructor(options: { baseUrl: any; email?: any; privateToken: any; requestTimeout: any; }) {
-    this.baseUrl = urlJoin(options.baseUrl, "/rest/api/3/");
+
+  constructor(options: { baseUrl: any; email?: any; privateToken: any; requestTimeout: any }) {
+    this.baseUrl = urlJoin(options.baseUrl, '/rest/api/3/');
     this.privateToken = options.privateToken;
     this.usermail = options.email;
     this.requestTimeout = options.requestTimeout;
     this.count = 0;
     this.stopping = false;
-
   }
 
-  getProjects() {} // can use this route to get projects paginated project/search
-
   getIssuesWithJQL(jql: string) {
-    //can use simple /search requests with jql and expand the results, should be no need to do a request for every single issue to get full data, max return size = 100
-    log("getIssuesWithJQL(%o)", jql);
-    const jqlSearchString = `jql=${encodeURIComponent(jql)}`; // part after the "jql=" needs to be encoded
+    //can use simple /search requests with jql and expand the results,
+    // should be no need to do a request for every single issue to get full data, max return size = 100
+    log('getIssuesWithJQL(%o)', jql);
+    const jqlSearchString = `fields=*all&jql=${encodeURIComponent(jql)}`; // part after the "jql=" needs to be encoded
 
-    return this.paginatedRequest(
-      "search?" + jqlSearchString
-    );
+    return this.paginatedRequest('search?' + jqlSearchString + '&');
+  }
+  getComments(issueKey: string) {
+    log('getComments(%o)', issueKey);
+    return this.paginatedRequest(`issue/${issueKey}/comment?`);
   }
 
   paginatedRequest(path: string) {
@@ -46,38 +46,36 @@ class Jira {
         if (this.stopping) {
           return Promise.resolve([]);
         }
-        return this.request(path + "&startAt=" + (start_at - 1));
+        return this.request(path + `startAt=${start_at}&maxResults=${per_request}`);
       },
       (resp: any) => {
-        return resp.body;
+        if (path.includes('/comment')) {
+          return resp.body.comments;
+        }
+        return resp.body.issues;
       },
       (resp: any) => {
-        return (this.count = parseInt(resp.headers.get("total"), 10));
+        return (this.count = parseInt(resp.body.total, 10));
       },
-      null
+      { its: 'jira' }
     );
   }
 
   async request(path: string) {
-
-    const credentials = this.usermail + ":" + this.privateToken;
+    const credentials = this.usermail + ':' + this.privateToken;
     const header = {
       method: 'GET',
       headers: {
-        'Authorization': `Basic ${Buffer.from(
-          credentials
-        ).toString('base64')}`,
-        'Accept': 'application/json',
-        "Accept-Language": "en_us"
-      }
-
+        Authorization: `Basic ${Buffer.from(credentials).toString('base64')}`,
+        Accept: 'application/json',
+        'Accept-Language': 'en_us',
+      },
     };
-    return fetch(urlJoin(this.baseUrl, path), header
-    ).then(async (response) => {
+    return fetch(urlJoin(this.baseUrl, path), header).then(async (response) => {
       const data = await response.json();
-      return {headers: response.headers, body: data};
+      return { headers: response.headers, body: data };
     });
   }
 }
 
-export default Jira
+export default Jira;
