7c2746b40a1ed7a2a7b10c5e7bbd1efc8f4a6687
Manuel Stöger <manuel.stoeger@inso.tuwien.ac.at>
2023-11-22 15:00:04 +0100

    Revert "#181: Set options for ifdef loader to 'offline'"
    This reverts commit 75d4bf8af60e0ee36b86ff3412a38f6891a40b96.

--- webpack.offline.js
+++ webpack.offline.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const opts = {
-  ENV: 'offline',
+  ENV: 'production',
 };
 
 export default merge(commonConfig, {
