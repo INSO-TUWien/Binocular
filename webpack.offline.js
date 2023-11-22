75d4bf8af60e0ee36b86ff3412a38f6891a40b96
Manuel Stöger <manuel.stoeger@inso.tuwien.ac.at>
2023-11-22 14:35:54 +0100

    #181: Set options for ifdef loader to 'offline'

--- webpack.offline.js
+++ webpack.offline.js
@@ -10,7 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const opts = {
-  ENV: 'production',
+  ENV: 'offline',
 };
 
 export default merge(commonConfig, {
