876d69bc405e89a37d1f057acb5ea581fbd5d541
Maximilian Zenz <e1633058@student.tuwien.ac.at>
2024-01-15 11:50:56 +0100

    #207 Backend Frontend separation
    - changing build behavior to only compile everything into one single file when building for offline execution

--- binocular-frontend/vite.config.ts
+++ binocular-frontend/vite.config.ts
@@ -10,105 +10,108 @@ import { viteSingleFile } from 'vite-plugin-singlefile';
 import ConditionalCompile from 'vite-plugin-conditional-compiler';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: './',
-  base: '',
-  server: {
-    port: 8080,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:48763/',
-        secure: false,
-      },
-      '/graphQl': {
-        target: 'http://localhost:48763/',
-        secure: false,
-        changeOrigin: true,
-      },
-      '/wsapi': {
-        target: 'ws://localhost:48763',
-        ws: true,
+export default () => {
+  const isOfflineBuild = () => process.env.VITE_OFFLINE === 'true';
+  return defineConfig({
+    root: './',
+    base: '',
+    server: {
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:48763/',
+          secure: false,
+        },
+        '/graphQl': {
+          target: 'http://localhost:48763/',
+          secure: false,
+          changeOrigin: true,
+        },
+        '/wsapi': {
+          target: 'ws://localhost:48763',
+          ws: true,
+        },
       },
     },
-  },
-  build: {
-    minify: false,
-    sourcemap: false, //generating the sourcemap uses way too much memory
-    commonjsOptions: {
-      include: /node_modules/,
-      requireReturnsDefault: 'auto',
-    },
-    emptyOutDir: true,
-    outDir: '../dist',
-    rollupOptions: {
-      cache: false,
-      treeshake: false,
-      output: {
-        format: 'esm',
+    build: {
+      minify: false,
+      sourcemap: false, //generating the sourcemap uses way too much memory
+      commonjsOptions: {
+        include: /node_modules/,
+        requireReturnsDefault: 'auto',
       },
-      plugins: [
-        nodeResolve({
-          browser: true,
-        }),
-      ],
+      emptyOutDir: true,
+      outDir: '../dist',
+      rollupOptions: {
+        cache: false,
+        treeshake: false,
+        output: {
+          format: 'esm',
+        },
+        plugins: [
+          nodeResolve({
+            browser: true,
+          }),
+        ],
+      },
     },
-  },
-  plugins: [
-    nodePolyfills(),
-    {
-      // source: https://stackblitz.com/edit/vitejs-vite-ka3qkc?file=vite.config.js
-      // source: https://github.com/vitejs/vite/discussions/3448#discussioncomment-5904031
-      name: 'load+transform-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/)) {
-          return null;
-        }
+    plugins: [
+      nodePolyfills(),
+      {
+        // source: https://stackblitz.com/edit/vitejs-vite-ka3qkc?file=vite.config.js
+        // source: https://github.com/vitejs/vite/discussions/3448#discussioncomment-5904031
+        name: 'load+transform-js-files-as-jsx',
+        async transform(code, id) {
+          if (!id.match(/src\/.*\.js$/)) {
+            return null;
+          }
 
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: 'jsx',
-          jsx: 'automatic',
-        });
+          // Use the exposed transform from vite, instead of directly
+          // transforming with esbuild
+          return transformWithEsbuild(code, id, {
+            loader: 'jsx',
+            jsx: 'automatic',
+          });
+        },
+      },
+      react(),
+      ConditionalCompile({ include: ['./src/database/localDB.js'] }),
+      isOfflineBuild() && viteSingleFile(),
+    ],
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer({}), // add options if needed
+        ],
       },
     },
-    react(),
-    ConditionalCompile({ include: ['./src/database/localDB.js'] }),
-    viteSingleFile(),
-  ],
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({}), // add options if needed
-      ],
-    },
-  },
-  optimizeDeps: {
-    disabled: 'build',
-    exclude: [],
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
+    optimizeDeps: {
+      disabled: 'build',
+      exclude: [],
+      esbuildOptions: {
+        loader: {
+          '.js': 'jsx',
+        },
+        // Enable esbuild polyfill plugins
+        plugins: [
+          NodeGlobalsPolyfillPlugin({
+            process: true,
+            buffer: true,
+          }),
+          NodeModulesPolyfillPlugin(),
+        ],
+        // Node.js global to browser globalThis
+        define: {
+          global: 'globalThis',
+        },
+        // resolveExtensions: ['.tsx', '.ts', '.js']
       },
-      // Enable esbuild polyfill plugins
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-        NodeModulesPolyfillPlugin(),
-      ],
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      // resolveExtensions: ['.tsx', '.ts', '.js']
     },
-  },
-  define: {
-    global: 'globalThis',
-  },
-  resolve: {
-    alias: viteAlias,
-  },
-});
+    define: {
+      global: 'globalThis',
+    },
+    resolve: {
+      alias: viteAlias,
+    },
+  });
+};
