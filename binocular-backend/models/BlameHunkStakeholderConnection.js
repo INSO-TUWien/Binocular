a18dee2c1a08dd1f9ce261d6181c3bb644a42f8b
Maximilian Zenz <e1633058@student.tuwien.ac.at>
2024-03-18 12:11:55 +0100

    #232 Convert all Backend Models to TS Classes
    - deleting unnecessary models and connection

--- binocular-backend/models/BlameHunkStakeholderConnection.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Connection from './Connection';
-import BlameHunk from './BlameHunk.js';
-import Stakeholder from './Stakeholder.js';
-
-const BlameHunkStakeholderConnection = new Connection(BlameHunk, Stakeholder);
-
-module.exports = BlameHunkStakeholderConnection;
--- binocular-backend/models/CommitBlameHunkConnection.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Connection = require('./Connection').default;
-const Commit = require('./Commit.js').default;
-const BlameHunk = require('./BlameHunk.js').default;
-
-const CommitBlameHunkConnection = new Connection(Commit, BlameHunk);
-
-module.exports = CommitBlameHunkConnection;
