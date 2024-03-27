ea2cd7696838d381e565849792e2d152f01d530c
Sebastian Watzinger <se.watzinger@gmail.com>
2024-03-27 12:54:06 +0100

    #235 refactored GitHub ITS indexer so issues and Merge Requests are processed by same function

--- binocular-backend/indexers/its/GitHubITSIndexer.ts
+++ binocular-backend/indexers/its/GitHubITSIndexer.ts
@@ -43,10 +43,93 @@ GitHubITSIndexer.prototype.configure = async function (config: any) {
 GitHubITSIndexer.prototype.index = async function () {
   let owner: string;
   let repo: string;
-  let omitIssueCount = 0;
-  let omitMergeRequestCount = 0;
-  let persistIssueCount = 0;
-  let persistMergeRequestCount = 0;
+
+  // helper function that persists the issues/mergeRequests and associated GitHub user accounts (plus connections)
+  const processIssues = async (issues: any, type: string, targetCollection: typeof Issue | typeof MergeRequest) => {
+    let persistCount = 0;
+    let omitCount = 0;
+
+    for (const issue of issues) {
+      log(`Processing ${type} #` + issue.number);
+
+      let issueEntry: Entry<IssueDataType | MergeRequestDataType>;
+
+      // create GitHub account objects for each relevant user (author, assignee, assignees)
+      const authorEntry: Entry<AccountDataType> = (await Account.ensureGitHubAccount(this.controller.getUser(issue.author.login)))[0];
+      const assigneeEntries: Entry<AccountDataType>[] = [];
+      for (const a of issue.assignees.nodes) {
+        assigneeEntries.push((await Account.ensureGitHubAccount(this.controller.getUser(a.login)))[0]);
+      }
+
+      // TODO these attributes are replaced by the connections. Remove later on
+      issue.author.name = this.controller.getUser(issue.author.login).name;
+      if (issue.assignees.nodes.length > 0) {
+        issue.assignees.nodes[0].name = this.controller.getUser(issue.assignees.nodes[0].login).name;
+      }
+      for (let i = 0; i < issue.assignees.nodes.length; i++) {
+        issue.assignees.nodes[i].name = this.controller.getUser(issue.assignees.nodes[i].login).name;
+      }
+
+      await targetCollection
+        .findOneByExample({ id: String(issue.id) })
+        .then((existingIssue) => {
+          if (!existingIssue || new Date(existingIssue.data.updatedAt).getTime() < new Date(issue.updatedAt).getTime()) {
+            log(`Processing ${type} #` + issue.iid);
+
+            return targetCollection
+              .persist({
+                id: issue.id.toString(),
+                iid: issue.number,
+                title: issue.title,
+                description: issue.body,
+                state: issue.state,
+                url: issue.url,
+                closedAt: issue.closedAt,
+                createdAt: issue.createdAt,
+                updatedAt: issue.updatedAt,
+                labels: issue.labels.nodes,
+                milestone: issue.milestone,
+                author: issue.author,
+                assignee: issue.assignees.nodes[0],
+                assignees: issue.assignees.nodes,
+                webUrl: issue.url,
+                mentions: issue.timelineItems.nodes.map((event: ItsIssueEvent) => {
+                  return {
+                    commit: event.commit ? event.commit.oid : null,
+                    createdAt: event.createdAt,
+                    closes: event.commit === undefined,
+                  } as Mention;
+                }),
+              })
+              .then(([persistedIssue, wasCreated]) => {
+                // save the entry object of the issue so we can connect it to the github users later
+                issueEntry = persistedIssue;
+                if (wasCreated) {
+                  persistCount++;
+                }
+                log(`Persisted ${type} #` + persistedIssue.data.iid);
+              });
+          } else {
+            // save the entry object of the issue so we can connect it to the github users later
+            issueEntry = existingIssue;
+            log(`Skipping ${type} #` + issue.iid);
+            omitCount++;
+          }
+        })
+        .then(() => {
+          // connect the issue/MR to the users (either as author, assignee or assignees)
+          if (type === 'issue') {
+            connectIssuesToUsers(IssueAccountConnection, issueEntry, authorEntry, assigneeEntries);
+            this.reporter.finishIssue();
+          } else if (type === 'mergeRequest') {
+            connectIssuesToUsers(MergeRequestAccountConnection, issueEntry, authorEntry, assigneeEntries);
+            this.reporter.finishMergeRequest();
+          }
+        });
+    }
+
+    log(`Persisted %d new ${type}s (%d already present)`, persistCount, omitCount);
+  };
 
   return Promise.resolve(this.repo.getOriginUrl()).then(async (url) => {
     if (url.includes('@')) {
@@ -66,161 +149,17 @@ GitHubITSIndexer.prototype.index = async function () {
     // Persist Issues and Authors/Assignees
     const issues = await this.controller.getIssuesWithEvents(owner, repo);
     this.reporter.setIssueCount(issues.length);
-
-    for (const issue of issues) {
-      log('Processing Issue #' + issue.number);
-
-      let issueEntry: Entry<IssueDataType>;
-      const authorEntry: Entry<AccountDataType> = (await Account.ensureGitHubAccount(this.controller.getUser(issue.author.login)))[0];
-      const assigneeEntries: Entry<AccountDataType>[] = [];
-
-      // create github account objects for each user
-      for (const a of issue.assignees.nodes) {
-        assigneeEntries.push((await Account.ensureGitHubAccount(this.controller.getUser(a.login)))[0]);
-      }
-
-      issue.author.name = this.controller.getUser(issue.author.login).name;
-      if (issue.assignees.nodes.length > 0) {
-        issue.assignees.nodes[0].name = this.controller.getUser(issue.assignees.nodes[0].login).name;
-      }
-
-      for (let i = 0; i < issue.assignees.nodes.length; i++) {
-        issue.assignees.nodes[i].name = this.controller.getUser(issue.assignees.nodes[i].login).name;
-      }
-      await Issue.findOneByExample({ id: String(issue.id) })
-        .then((existingIssue) => {
-          if (!existingIssue || new Date(existingIssue.data.updatedAt).getTime() < new Date(issue.updatedAt).getTime()) {
-            log('Processing issue #' + issue.iid);
-
-            return Issue.persist({
-              id: issue.id.toString(),
-              iid: issue.number,
-              title: issue.title,
-              description: issue.body,
-              state: issue.state,
-              url: issue.url,
-              closedAt: issue.closedAt,
-              createdAt: issue.createdAt,
-              updatedAt: issue.updatedAt,
-              labels: issue.labels.nodes,
-              milestone: issue.milestone,
-              author: issue.author,
-              assignee: issue.assignees.nodes[0],
-              assignees: issue.assignees.nodes,
-              webUrl: issue.url,
-              mentions: issue.timelineItems.nodes.map((event: ItsIssueEvent) => {
-                return {
-                  commit: event.commit ? event.commit.oid : null,
-                  createdAt: event.createdAt,
-                  closes: event.commit === undefined,
-                } as Mention;
-              }),
-            }).then(([persistedIssue, wasCreated]) => {
-              // save the entry object of the issue so we can connect it to the github users later
-              issueEntry = persistedIssue;
-              if (wasCreated) {
-                persistIssueCount++;
-              }
-              log('Persisted issue #' + persistedIssue.data.iid);
-            });
-          } else {
-            // save the entry object of the issue so we can connect it to the github users later
-            issueEntry = existingIssue;
-            log('Skipping issue #' + issue.iid);
-            omitIssueCount++;
-          }
-        })
-        .then(() => {
-          // connect the issue to the users (either as author, assignee or assignees)
-          connectIssuesToUsers(IssueAccountConnection, issueEntry, authorEntry, assigneeEntries);
-          this.reporter.finishIssue();
-        });
-    }
-
-    log('Persisted %d new issues (%d already present)', persistIssueCount, omitIssueCount);
+    await processIssues(issues, 'issue', Issue);
 
     // Persist Merge Requests and Authors/Assignees
     const mergeRequests = await this.controller.getPullRequestsWithEvents(owner, repo);
     this.reporter.setMergeRequestCount(mergeRequests.length);
-
-    for (const mergeRequest of mergeRequests) {
-      log('Processing Issue #' + mergeRequest.number);
-
-      // create github account objects for each user
-      let mrEntry: Entry<IssueDataType>;
-      const authorEntry: Entry<AccountDataType> = (
-        await Account.ensureGitHubAccount(this.controller.getUser(mergeRequest.author.login))
-      )[0];
-      const assigneeEntries: Entry<AccountDataType>[] = [];
-
-      for (const a of mergeRequest.assignees.nodes) {
-        assigneeEntries.push((await Account.ensureGitHubAccount(this.controller.getUser(a.login)))[0]);
-      }
-
-      mergeRequest.author.name = this.controller.getUser(mergeRequest.author.login).name;
-      if (mergeRequest.assignees.nodes.length > 0) {
-        mergeRequest.assignees.nodes[0].name = this.controller.getUser(mergeRequest.assignees.nodes[0].login).name;
-      }
-
-      for (let i = 0; i < mergeRequest.assignees.nodes.length; i++) {
-        mergeRequest.assignees.nodes[i].name = this.controller.getUser(mergeRequest.assignees.nodes[i].login).name;
-      }
-      await MergeRequest.findOneByExample({ id: String(mergeRequest.id) })
-        .then((existingMergeRequest) => {
-          if (
-            !existingMergeRequest ||
-            new Date(existingMergeRequest.data.updatedAt).getTime() < new Date(mergeRequest.updatedAt).getTime()
-          ) {
-            log('Processing issue #' + mergeRequest.iid);
-            return MergeRequest.persist({
-              id: mergeRequest.id.toString(),
-              iid: mergeRequest.number,
-              title: mergeRequest.title,
-              description: mergeRequest.body,
-              state: mergeRequest.state,
-              url: mergeRequest.url,
-              closedAt: mergeRequest.closedAt,
-              createdAt: mergeRequest.createdAt,
-              updatedAt: mergeRequest.updatedAt,
-              labels: mergeRequest.labels.nodes,
-              milestone: mergeRequest.milestone,
-              author: mergeRequest.author,
-              assignee: mergeRequest.assignees.nodes[0],
-              assignees: mergeRequest.assignees.nodes,
-              webUrl: mergeRequest.url,
-              mentions: mergeRequest.timelineItems.nodes.map((event: ItsIssueEvent) => {
-                return {
-                  commit: event.commit ? event.commit.oid : null,
-                  createdAt: event.createdAt,
-                  closes: event.commit === undefined,
-                } as Mention;
-              }),
-            }).then(([persistedMergeRequest, wasCreated]) => {
-              // save the entry object of the issue so we can connect it to the github users later
-              mrEntry = persistedMergeRequest;
-              if (wasCreated) {
-                persistMergeRequestCount++;
-              }
-              log('Persisted mergeRequest #' + persistedMergeRequest.data.iid);
-            });
-          } else {
-            mrEntry = existingMergeRequest;
-            log('Skipping mergeRequest #' + mergeRequest.iid);
-            omitMergeRequestCount++;
-          }
-        })
-        .then(() => {
-          // connect the MR to the users (either as author, assignee or assignees)
-          connectIssuesToUsers(MergeRequestAccountConnection, mrEntry, authorEntry, assigneeEntries);
-          this.reporter.finishMergeRequest();
-        });
-    }
-    log('Persisted %d new mergeRequests (%d already present)', persistMergeRequestCount, omitMergeRequestCount);
+    await processIssues(mergeRequests, 'mergeRequest', MergeRequest);
   });
 };
 
 // connects issues or merge requests to accounts
-const connectIssuesToUsers = (
+const connectIssuesToUsers = async (
   conn: Connection<
     IssueAccountConnectionDataType | MergeRequestAccountConnectionDataType,
     IssueDataType | MergeRequestDataType,
@@ -230,13 +169,15 @@ const connectIssuesToUsers = (
   author: Entry<AccountDataType>,
   assignees: Entry<AccountDataType>[],
 ) => {
-  conn.ensureWithData({ role: 'author' }, { from: issue, to: author });
+  await conn.ensureWithData({ role: 'author' }, { from: issue, to: author });
   if (assignees.length > 0) {
-    IssueAccountConnection.ensureWithData({ role: 'assignee' }, { from: issue, to: assignees[0] });
+    await conn.ensureWithData({ role: 'assignee' }, { from: issue, to: assignees[0] });
   }
-  assignees.map((ae) => {
-    IssueAccountConnection.ensureWithData({ role: 'assignees' }, { from: issue, to: ae });
-  });
+  await Promise.all(
+    assignees.map(async (ae) => {
+      await conn.ensureWithData({ role: 'assignees' }, { from: issue, to: ae });
+    }),
+  );
 };
 
 GitHubITSIndexer.prototype.isStopping = function () {
