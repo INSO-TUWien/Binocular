ab41ec14b9309c22eab9781276d2d9ff079f3150
Maximilian Zenz <e1633058@student.tuwien.ac.at>
2024-04-18 11:29:11 +0200

    #180 Frontend Redesign
    - adding tailwind and daisyUI
    - adding basic binocular theme

--- binocular-frontend-new/src/components/tabMenu/tab/tab.tsx
+++ binocular-frontend-new/src/components/tabMenu/tab/tab.tsx
@@ -3,6 +3,7 @@ function Tab(props: { displayName: string; defaultAlignment: string }) {
   return (
     <>
       <div className={tabStyles.tab}>{props.displayName}-Content</div>
+      <button className={'btn btn-accent'}>Hello daisyUI</button>
     </>
   );
 }
