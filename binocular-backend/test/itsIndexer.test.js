cead46f364adb64927994ae0cc5583aee108f7d5
Sebastian Watzinger <se.watzinger@gmail.com>
2024-05-26 13:34:15 +0200

    #235 GitLab: ITS indexer tests
    added tests that check if the indexer behaves correctly when certain GitLab api calls return empty responses

--- binocular-backend/test/itsIndexer.test.js
+++ binocular-backend/test/itsIndexer.test.js
@@ -64,6 +64,7 @@ describe('its', function () {
     sinon.restore();
   });
 
+  // remaps the git functions that would fetch infos from remote repos to the ones that only check the local git information.
   const remapRemoteFunctions = (repo) => {
     repo.listAllCommitsRemote = repo.listAllCommits;
     repo.getAllBranchesRemote = repo.getAllBranches;
@@ -130,7 +131,11 @@ describe('its', function () {
   };
 
   describe('#indexGitLab', function () {
-    it('should index all GitLab issues and create all necessary db collections and connections', async function () {
+    // GITLAB HELPER FUNCTIONS
+
+    // basic setup for gitlab. Returns the gitlab indexer.
+    // Sets up the default gitlab mock implementation.
+    const gitlabSetup = async () => {
       const repo = await fake.repository();
       ctx.targetPath = repo.path;
 
@@ -145,6 +150,126 @@ describe('its', function () {
 
       const gitLabITSIndexer = new GitLabITSIndexer(repo, reporter);
       gitLabITSIndexer.configure(config);
+
+      return gitLabITSIndexer;
+    };
+
+    // the gitlab mock implementation returns some standard test data.
+    // But for some tests, we need the mock api to return something else.
+    const remapGitlabApiCall = (indexer, functionName, newData) => {
+      if (indexer.gitlab === null || indexer.gitlab === undefined) {
+        throw Error('Test Error: indexer.gitlab does not exist, function "' + functionName + '" cannot be remapped');
+      }
+      const fun = indexer.gitlab[functionName];
+      if (fun === null || fun === undefined) {
+        throw Error('Test Error: function "' + functionName + '" does not exist, cannot be remapped');
+      }
+      indexer.gitlab[functionName] = () => {
+        return indexer.gitlab.testPaginator(newData);
+      };
+    };
+
+    // some helper functions that check if the expected data has been indexed when the default gitlab mock implementation is used.
+    const expectDefaultIssues = (collections, checkAccounts = true, checkMilestones = true, checkNotes = true) => {
+      // issues
+      expect(collections['issues'].length).to.equal(3);
+
+      // issues-accounts
+      if (checkAccounts) {
+        const accId = collections['accounts'][0]._id;
+        for (const issue of collections['issues']) {
+          // we expect the only account there is to have roles author, assignee and assignees
+          expectExamples({ _from: issue._id, _to: accId, role: 'author' }, collections['issues-accounts'], 1);
+          expectExamples({ _from: issue._id, _to: accId, role: 'assignee' }, collections['issues-accounts'], 1);
+          expectExamples({ _from: issue._id, _to: accId, role: 'assignees' }, collections['issues-accounts'], 1);
+        }
+      }
+
+      // issues-milestones
+      if (checkMilestones) {
+        const milestoneId = collections['milestones'][0]._id;
+        for (const issue of collections['issues']) {
+          // every issue is connected to the only milestone there is
+          expectExamples({ _from: issue._id, _to: milestoneId }, collections['issues-milestones'], 1);
+        }
+      }
+
+      // issues-notes
+      if (checkNotes) {
+        for (const issue of collections['issues']) {
+          expect(issue.mentions.length).to.equal(2);
+          expect(issue.mentions[0].closes).to.equal(true);
+          expect(issue.mentions[1].commit).to.equal('1234567890');
+          expectExamples({ _from: issue._id }, collections['issues-notes'], 3);
+          const issueNoteConnections = expectExamples({ _from: issue._id }, collections['issues-notes'], 3);
+          for (const conn of issueNoteConnections) {
+            // for each note connected to this issue, we expect to have one connection to an account
+            expectExamples({ _from: conn._to }, collections['notes-accounts'], 1);
+          }
+        }
+      }
+    };
+    const expectDefaultMergeRequests = (collections, checkAccounts = true, checkMilestones = true, checkNotes = true) => {
+      // mergeRequests
+      expect(collections['mergeRequests'].length).to.equal(3);
+
+      // mergeRequests-accounts
+      if (checkAccounts) {
+        const accId = collections['accounts'][0]._id;
+        for (const mr of collections['mergeRequests']) {
+          // we expect the only account there is to have roles author, assignee and assignees
+          expectExamples({ _from: mr._id, _to: accId, role: 'author' }, collections['mergeRequests-accounts'], 1);
+          expectExamples({ _from: mr._id, _to: accId, role: 'assignee' }, collections['mergeRequests-accounts'], 1);
+          expectExamples({ _from: mr._id, _to: accId, role: 'assignees' }, collections['mergeRequests-accounts'], 1);
+        }
+      }
+
+      // mergeRequests-milestones
+      if (checkMilestones) {
+        const milestoneId = collections['milestones'][0]._id;
+        for (const mr of collections['mergeRequests']) {
+          // every issue is connected to the only milestone there is
+          expectExamples({ _from: mr._id, _to: milestoneId }, collections['mergeRequests-milestones'], 1);
+        }
+      }
+
+      // mergeRequests-notes
+      if (checkNotes) {
+        for (const mr of collections['mergeRequests']) {
+          expectExamples({ _from: mr._id }, collections['mergeRequests-notes'], 3);
+          const mrNoteConnections = expectExamples({ _from: mr._id }, collections['mergeRequests-notes'], 3);
+          for (const conn of mrNoteConnections) {
+            // for each note connected to this mr, we expect to have one connection to an account
+            expectExamples({ _from: conn._to }, collections['notes-accounts'], 1);
+          }
+        }
+      }
+    };
+    const expectDefaultNotes = (collections) => {
+      let noteCounter = 0;
+      for (const issue of collections['issues']) {
+        const issueNoteConnections = expectExamples({ _from: issue._id }, collections['issues-notes'], 3);
+        noteCounter += issueNoteConnections.length;
+      }
+      // mergeRequests-notes
+      for (const mr of collections['mergeRequests']) {
+        const mrNoteConnections = expectExamples({ _from: mr._id }, collections['mergeRequests-notes'], 3);
+        noteCounter += mrNoteConnections.length;
+      }
+      // we don't want notes that are not connected to any issue/mr
+      expect(collections['notes'].length).to.equal(noteCounter);
+    };
+    const expectDefaultAccounts = (collections) => {
+      expect(collections['accounts'].length).to.equal(1);
+    };
+    const expectDefaultMilestones = (collections) => {
+      expect(collections['milestones'].length).to.equal(1);
+    };
+
+    // TESTS
+
+    it('should index all GitLab issues and create all necessary db collections and connections', async function () {
+      const gitLabITSIndexer = await gitlabSetup();
 
       // start indexer. gets data from GitLab Mock implementation (see ./gitlab)
       await gitLabITSIndexer.index();
@@ -153,99 +278,20 @@ describe('its', function () {
       const collections = await getAllCollections();
 
       // check if the data was indexed as expected:
-
-      // accounts
-      expect(collections['accounts'].length).to.equal(1);
-
-      // milestones
-      expect(collections['milestones'].length).to.equal(1);
-
       // issues
-      expect(collections['issues'].length).to.equal(3);
-      for (const issue of collections['issues']) {
-        expect(issue.mentions.length).to.equal(2);
-        expect(issue.mentions[0].closes).to.equal(true);
-        expect(issue.mentions[1].commit).to.equal('1234567890');
-        expectExamples({ _from: issue._id }, collections['issues-notes'], 3);
-      }
-
+      expectDefaultIssues(collections);
       // mergeRequests
-      expect(collections['mergeRequests'].length).to.equal(3);
-      for (const mergeRequest of collections['mergeRequests']) {
-        expectExamples({ _from: mergeRequest._id }, collections['mergeRequests-notes'], 3);
-      }
-
-      // issues-accounts
-      const accId = collections['accounts'][0]._id;
-      for (const issue of collections['issues']) {
-        // we expect the only account there is to have roles author, assignee and assignees
-        expectExamples({ _from: issue._id, _to: accId, role: 'author' }, collections['issues-accounts'], 1);
-        expectExamples({ _from: issue._id, _to: accId, role: 'assignee' }, collections['issues-accounts'], 1);
-        expectExamples({ _from: issue._id, _to: accId, role: 'assignees' }, collections['issues-accounts'], 1);
-      }
-
-      // mergeRequests-accounts
-      for (const mr of collections['mergeRequests']) {
-        // we expect the only account there is to have roles author, assignee and assignees
-        expectExamples({ _from: mr._id, _to: accId, role: 'author' }, collections['mergeRequests-accounts'], 1);
-        expectExamples({ _from: mr._id, _to: accId, role: 'assignee' }, collections['mergeRequests-accounts'], 1);
-        expectExamples({ _from: mr._id, _to: accId, role: 'assignees' }, collections['mergeRequests-accounts'], 1);
-      }
-
-      // issues-milestones
-      const milestoneId = collections['milestones'][0]._id;
-      for (const issue of collections['issues']) {
-        // every issue is connected to the only milestone there is
-        expectExamples({ _from: issue._id, _to: milestoneId }, collections['issues-milestones'], 1);
-      }
-
-      // mergeRequests-milestones
-      for (const mr of collections['mergeRequests']) {
-        // every issue is connected to the only milestone there is
-        expectExamples({ _from: mr._id, _to: milestoneId }, collections['mergeRequests-milestones'], 1);
-      }
-
-      let noteCounter = 0;
-
-      // issues-notes
-      for (const issue of collections['issues']) {
-        const issueNoteConnections = expectExamples({ _from: issue._id }, collections['issues-notes'], 3);
-        for (const conn of issueNoteConnections) {
-          noteCounter++;
-          // for each note connected to this issue, we expect to have one connection to an account
-          expectExamples({ _from: conn._to }, collections['notes-accounts'], 1);
-        }
-      }
-
-      // mergeRequests-notes
-      for (const mr of collections['mergeRequests']) {
-        const mrNoteConnections = expectExamples({ _from: mr._id }, collections['mergeRequests-notes'], 3);
-        for (const conn of mrNoteConnections) {
-          noteCounter++;
-          // for each note connected to this mr, we expect to have one connection to an account
-          expectExamples({ _from: conn._to }, collections['notes-accounts'], 1);
-        }
-      }
-
-      // we don't want notes that are not connected to any issue/mr
-      expect(collections['notes'].length).to.equal(noteCounter);
+      expectDefaultMergeRequests(collections);
+      // milestones
+      expectDefaultMilestones(collections);
+      // accounts
+      expectDefaultAccounts(collections);
+      // notes
+      expectDefaultNotes(collections);
     });
 
     it('should not persist objects twice if indexer is called twice (GitLab)', async function () {
-      const repo = await fake.repository();
-      ctx.targetPath = repo.path;
-
-      //Remap Remote functions to local ones because remote repository doesn't exist anymore.
-      remapRemoteFunctions(repo);
-      repo.getOriginUrl = async function () {
-        return 'git@gitlab.com:Test/Test-Project.git';
-      };
-
-      // init all relevant collections for ITS indexing
-      await setupDb(db, relevantCollections);
-
-      const gitLabITSIndexer = new GitLabITSIndexer(repo, reporter);
-      gitLabITSIndexer.configure(config);
+      const gitLabITSIndexer = await gitlabSetup();
 
       // start indexer. gets data from GitLab Mock implementation (see ./gitlab)
       await gitLabITSIndexer.index();
@@ -261,6 +307,71 @@ describe('its', function () {
         // check if updated collection has the same size
         expect(collectionArray.length).to.equal(updatedCollections[collectionName].length);
       });
+    });
+
+    it('can handle empty milestone collection', async function () {
+      const gitLabITSIndexer = await gitlabSetup();
+      remapGitlabApiCall(gitLabITSIndexer, 'getMileStones', []);
+      // start indexer. gets data from GitLab Mock implementation (see ./gitlab)
+      await gitLabITSIndexer.index();
+
+      // get all entries from all relevant collections
+      const collections = await getAllCollections();
+
+      // other collections should be unaffected (except connections to milestones)
+      expectDefaultIssues(collections, true, false, true);
+      expectDefaultMergeRequests(collections, true, false, true);
+      expectDefaultAccounts(collections);
+      expectDefaultNotes(collections);
+    });
+
+    it('can handle empty issues collection', async function () {
+      const gitLabITSIndexer = await gitlabSetup();
+      remapGitlabApiCall(gitLabITSIndexer, 'getIssues', []);
+      // start indexer. gets data from GitLab Mock implementation (see ./gitlab)
+      await gitLabITSIndexer.index();
+
+      // get all entries from all relevant collections
+      const collections = await getAllCollections();
+
+      // other collections should be unaffected
+      expectDefaultMergeRequests(collections);
+      expectDefaultMilestones(collections);
+      expectDefaultAccounts(collections);
+      expectDefaultNotes(collections);
+    });
+
+    it('can handle empty getMileStones collection', async function () {
+      const gitLabITSIndexer = await gitlabSetup();
+      remapGitlabApiCall(gitLabITSIndexer, 'getMergeRequests', []);
+      // start indexer. gets data from GitLab Mock implementation (see ./gitlab)
+      await gitLabITSIndexer.index();
+
+      // get all entries from all relevant collections
+      const collections = await getAllCollections();
+
+      // other collections should be unaffected
+      expectDefaultIssues(collections);
+      expectDefaultMilestones(collections);
+      expectDefaultAccounts(collections);
+      expectDefaultNotes(collections);
+    });
+
+    it('can handle empty notes collection', async function () {
+      const gitLabITSIndexer = await gitlabSetup();
+      remapGitlabApiCall(gitLabITSIndexer, 'getNotes', []);
+      remapGitlabApiCall(gitLabITSIndexer, 'getMergeRequestNotes', []);
+      // start indexer. gets data from GitLab Mock implementation (see ./gitlab)
+      await gitLabITSIndexer.index();
+
+      // get all entries from all relevant collections
+      const collections = await getAllCollections();
+
+      // other collections should be unaffected
+      expectDefaultIssues(collections, true, true, false);
+      expectDefaultMergeRequests(collections, true, true, false);
+      expectDefaultMilestones(collections);
+      expectDefaultAccounts(collections);
     });
   });
 
