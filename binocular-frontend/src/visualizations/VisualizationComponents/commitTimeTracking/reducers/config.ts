dd071ebde90775c688679d8901e5fb7a65fae9a5
TBalint2000 <e12121289@student.tuwien.ac.at>
2024-06-19 10:32:25 +0200

    feat: implement reducers for chart options
    Ref: #237
    Time-spent: 0h14m

--- binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/reducers/config.ts
+++ binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/reducers/config.ts
@@ -1,5 +1,20 @@
 'use strict';
 
-import { handleActions } from 'redux-actions';
+import { Action, handleActions } from 'redux-actions';
+import * as _ from 'lodash';
 
-export default handleActions({}, {}); //TODO: replace with reducers for config
+export default handleActions(
+  {
+    SET_SELECTED_AUTHORS: (state, action: Action<any>) => _.assign({}, state, { selectedAuthors: [...action.payload] }),
+    SET_SELECTED_BRANCH: (state, action: Action<any>) => _.assign({}, state, { selectedBranch: action.payload }),
+    SET_SELECTED_COMMIT_TYPE: (state, action: Action<any>) => _.assign({}, state, { commitType: action.payload }),
+    SET_THRESHOLD: (state, action: Action<any>) => _.assign({}, state, { threshold: { ...action.payload } }),
+  },
+  {
+    selectedAuthors: [], //Authors checked in the CheckBoxLegend, Array of objects: [{id: 1234, gitSignature: 'Dev1 <Dev1@email.com>'}, ...]
+    availableAuthors: [], //All authors that should be displayed in CheckBoxLegend, Same format as above
+    selectedBranch: 'main', //Branch to be displayed
+    commitType: 'all', //Filter to display relevant commits, can be 'all' or any of the commit types
+    threshold: {}, //Filter for threshold for commits based on line changes, time spent and their ratio
+  },
+);
--- binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/reducers/data.ts
+++ binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/reducers/data.ts
@@ -1,5 +1,20 @@
 'use strict';
 
-import { handleActions } from 'redux-actions';
+import { Action, handleActions } from 'redux-actions';
+import * as _ from 'lodash';
 
-export default handleActions({}, {}); //TODO: replace with reducers for fetching the data
+export default handleActions(
+  {
+    REQUEST_CHANGES_DATA: (state) => _.assign({}, state, { isFetching: true }),
+    RECEIVE_CHANGES_DATA: (state, action: Action<any>) => {
+      return _.assign({}, state, {
+        data: action.payload,
+        isFetching: false,
+      });
+    },
+  },
+  {
+    data: {},
+    isFetching: null,
+  },
+);
