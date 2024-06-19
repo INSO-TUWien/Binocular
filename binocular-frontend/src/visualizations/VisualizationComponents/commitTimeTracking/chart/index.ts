4ed087584c0b8bd60e470b42041f8c7fd1536771
TBalint2000 <e12121289@student.tuwien.ac.at>
2024-06-19 12:11:48 +0200

    feat: implemented index.ts for chart folder
    Ref: #237
    Time-spent: 0h20m

--- binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/chart/index.ts
+++ binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/chart/index.ts
@@ -3,14 +3,62 @@
 import { connect } from 'react-redux';
 import Chart from './chart.tsx';
 import { GlobalState } from '../../../../types/globalTypes.ts';
+import moment from 'moment';
+import { Commit } from '../../../../types/commitTypes.ts';
+import { Author, Committer, Palette } from '../../../../types/authorTypes.ts';
 
-interface Props {} //TODO: replace empty interface
+interface Props {
+  chartResolution: moment.unitOfTime.DurationConstructor;
+  commits: Commit[];
+  filteredCommits: Commit[];
+  committers: string[];
+  excludeMergeCommits: boolean;
+  excludedCommits: string[];
+  excludeCommits: boolean;
+  firstCommitTimestamp: number;
+  lastCommitTimestamp: number;
+  firstSignificantTimestamp: number;
+  lastSignificantTimestamp: number;
+  selectedBranch: string;
+  commitType: string;
+  threshold: {
+    hours: { lower: number; upper: number };
+    time: { lower: number; upper: number };
+    ratio: { lower: number; upper: number };
+  };
+  mergedAuthors: Author[];
+  otherAuthors: Committer[];
+  otherCount: number;
+  palette: Palette;
+  selectedAuthors: string[];
+}
 const mapStateToProps = (state: GlobalState): Props => {
-  return {}; //TODO: replace function with actual mapping
+  const timeTrackingState = state.visualizations.commitTimeTracking.state;
+  const universalSettings = state.universalSettings;
+  return {
+    palette: timeTrackingState.data.data.palette,
+    otherCount: timeTrackingState.data.data.otherCount,
+    filteredCommits: timeTrackingState.data.data.filteredCommits,
+    commits: timeTrackingState.data.data.commits,
+    committers: timeTrackingState.data.data.committers,
+    firstCommitTimestamp: timeTrackingState.data.data.firstCommitTimestamp,
+    lastCommitTimestamp: timeTrackingState.data.data.lastCommitTimestamp,
+    firstSignificantTimestamp: timeTrackingState.data.data.firstSignificantTimestamp,
+    lastSignificantTimestamp: timeTrackingState.data.data.lastSignificantTimestamp,
+    selectedBranch: timeTrackingState.config.selectedBranch,
+    commitType: timeTrackingState.config.commitType,
+    threshold: timeTrackingState.config.threshold,
+    selectedAuthors: universalSettings.selectedAuthorsGlobal,
+    otherAuthors: universalSettings.otherAuthors,
+    mergedAuthors: universalSettings.mergedAuthors,
+    chartResolution: universalSettings.chartResolution,
+    excludeMergeCommits: universalSettings.excludeMergeCommits,
+    excludedCommits: universalSettings.excludedCommits,
+    excludeCommits: universalSettings.excludeCommits,
+  };
 };
-
 const mapDispatchToProps = () => {
-  return {}; //TODO: replace function with actual mapping
+  return {};
 };
 
 // @ts-ignore
