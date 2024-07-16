82c7201b7e19a98c3b134e74e03ebfbff5364ff1
TBalint2000 <e12121289@student.tuwien.ac.at>
2024-07-16 23:45:29 +0200

    feat: implement initial graph
    The first draft of the graph was implemented. This includes both barcharts with the nodes in the middle.
    
    A lot of values in styling are hardcoded and the graph is not responsive. Both aspects remain to be corrected.
    
    Furthermore, the hover-over for the nodes for more information has to be also implemented.
    
    Potential for additional work would be the adjustment of the bars depending on the probability of the label.
    
    Ref:#237
    Time-spent: 4h0m

--- binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/chart/chart.tsx
+++ binocular-frontend/src/visualizations/VisualizationComponents/commitTimeTracking/chart/chart.tsx
@@ -3,6 +3,7 @@ import {Commit} from "../../../../types/commitTypes.ts";
 import getCommitType from '../../../../utils/getCommitType.ts';
 import * as React from "react";
 import _ from 'lodash';
+import * as d3 from 'd3';
 
 interface Props {
   commits: Commit[];
@@ -33,7 +34,7 @@ interface CommitChartData {
   commitMessage: string;
   author: string;
   date: Date;
-  commitType: string[];
+  commitType: [{label: string; value: number}];
 }
 
 export default (props: Props) => {
@@ -52,8 +53,141 @@ export default (props: Props) => {
     fetchData();
   }, [props]);
 
-  return (<div>Content</div>);
+  React.useEffect(() => {
+    if (commitChartData.length > 0) {
+      console.log(commitChartData);
+      drawChart(commitChartData);
+    }
+  }, [commitChartData]);
+
+
+
+  const drawChart = (data: CommitChartData[]) => {
+    drawUpperChart(data);
+    drawNodeChart(data);
+    drawLowerChart(data);
+  };
+
+  return (
+    <span style={{height: '100%'}}>
+      <div id="upperChart" style={{height: 'calc(50% - 20px)'}}>upperChart</div>
+      <div id="nodeChart" style={{height: '40px'}}> Nodes </div>
+      <div id="lowerChart" style={{height: 'calc(50% - 20px)'}}>lowerChart</div>
+    </span>
+  );
 };
+
+function drawUpperChart(data: CommitChartData[]) {
+  const margin = {top: 40, right: 30, bottom: 0, left: 40};
+  const width = visualViewport?.width - 511 - margin.left - margin.right;
+  const height = 449.5 - margin.top;
+
+  const svg = d3.select("#upperChart")
+    .html("")
+    .append("svg")
+    .attr("width", '100%')
+    .attr("height", '100%')
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.date.toString()))
+    .range([0, width])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.timeSpent.estimated)])
+    .nice()
+    .range([height, 0]);
+
+  const color = d3.scaleOrdinal()
+    .domain(['corrective', 'features', 'unknown', 'nonfunctional', 'perfective'])
+    .range(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']);
+
+  svg.append("g")
+    .selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d.date.toString()))
+    .attr("y", d => y(d.timeSpent.estimated))
+    .attr("width", x.bandwidth())
+    .attr("height", d => height - y(d.timeSpent.estimated))
+    .attr("fill", d => color(d.commitType[0].label));
+}
+
+function drawNodeChart(data: CommitChartData[]) {
+  const margin = { top: 5, right: 30, bottom: 5, left: 40 };
+  const width = visualViewport?.width - 511 - margin.left - margin.right || 600; // Fallback width
+  const height = 40;
+
+  const svg = d3.select("#nodeChart")
+    .html("")
+    .append("svg")
+    .attr("width", '100%')
+    .attr("height", height)
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  // Create a scale for x based on the number of nodes
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.date.toString()))
+    .range([0, width])
+    .padding(0.1);
+
+  // Append circles (nodes) along the x-axis
+  svg.selectAll(".node")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("class", "node")
+    .attr("cx", d => x(d.date.toString()) + x.bandwidth() / 2) // Center the node along x-axis
+    .attr("cy", height / 2 - x.bandwidth() / 3) // Place nodes at the middle of the chart
+    .attr("r", x.bandwidth() / 2) // Radius of the nodes
+    .attr("stroke", "#000") // Add a border around nodes
+    .attr("stroke-width", 1); // Width of the border
+}
+
+function drawLowerChart(data: CommitChartData[]) {
+  const margin = {top: 0, right: 30, bottom: 40, left: 40};
+  const width = visualViewport?.width - 511 - margin.left - margin.right;
+  const height = 449.5 - margin.bottom; // Define the height of the chart area
+
+  const svg = d3.select("#lowerChart")
+    .html("")
+    .append("svg")
+    .attr("width", '100%')
+    .attr("height", '100%')
+    .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  const x = d3.scaleBand()
+    .domain(data.map(d => d.date.toString()))
+    .range([0, width])
+    .padding(0.1);
+
+  const y = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.timeSpent.estimated)])
+    .nice()
+    .range([0, height]);
+
+  const color = d3.scaleOrdinal()
+    .domain(['corrective', 'features', 'unknown', 'nonfunctional', 'perfective'])
+    .range(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']);
+
+  svg.append("g")
+    .selectAll(".bar")
+    .data(data)
+    .enter()
+    .append("rect")
+    .attr("class", "bar")
+    .attr("x", d => x(d.date.toString()))
+    .attr("y", 0) // Set y to 0 to start from the top
+    .attr("width", x.bandwidth())
+    .attr("height", d => y(d.timeSpent.estimated)) // Height is directly proportional to the estimated time spent
+    .attr("fill", d => color(d.commitType[0].label));
+}
 
 const extractCommitData = async (props: Props): Promise<{ commitChartData: CommitChartData[]; maxTime: { estimated: number; actual: number; }; maxChange: number; }> => {
   if (!props.commits || props.commits.length === 0) {
