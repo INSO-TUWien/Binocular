c97158809354e3776d214c783c4a32cbd1fb75b4
maerzman <20152088@atn.ac.at>
2024-09-05 16:09:11 +0200

    #233 added help component descriptions

--- binocular-frontend/src/visualizations/merge-request-lifecycle/help.tsx
+++ binocular-frontend/src/visualizations/merge-request-lifecycle/help.tsx
@@ -8,8 +8,26 @@ export default class HelpComponent extends React.Component {
   render() {
     return (
       <div className={cx('box', styles.help)}>
-        <h1 className="title">Dashboard Help</h1>
-        <p>Some helpful text.</p>
+        <h1>MergeRequest Lifecycle Help</h1>
+        <p>This chart shows the amount of time it took for a MergeRequest from being opend to ending up closed or merged.</p>
+        <h2>Interaction</h2>
+        <p>Hovering over the respective bubble displays additional info</p>
+        <p>
+          <i className="fa fa-times" style={{ transform: 'rotate(45deg)' }} /> Select an area of the chart to zoom.
+        </p>
+        <h2>Grouping</h2>
+        <ul>
+          <li>
+            <strong>Single:</strong> Each bubble corresponds to a single MergeRequest. The color depicts the state of the MergeRequest while
+            the x-Axis depicts the amount of time it spent from open to being closed/merged.
+          </li>
+          <li>
+            <strong>Category:</strong> Shows the amount of MergeRequest in each state.
+          </li>
+          <li>
+            <strong>Cumulative:</strong> Groups identical values according to the granularity specified in Granularity tab.
+          </li>
+        </ul>
       </div>
     );
   }
--- binocular-frontend/src/visualizations/merge-request-ownership/help.tsx
+++ binocular-frontend/src/visualizations/merge-request-ownership/help.tsx
@@ -8,7 +8,30 @@ export default class HelpComponent extends React.Component {
   render() {
     return (
       <div className={cx('box', styles.help)}>
-        <p>Some helpful text.</p>
+        <h1>MergeRequest Ownership Help</h1>
+        <p>This chart shows the amount of MergeRequests a user is assigned to as either an assignee or reviewer.</p>
+
+        <h2>Interaction</h2>
+        <p>Hovering over the respective bubble shows the login of the specified user.</p>
+
+        <h2>Sidebar</h2>
+        <ul>
+          <li>
+            <strong>Date Range:</strong> Select a Date Range which contains all the MergeRequests inside this range.
+          </li>
+          <li>
+            <strong>Author Selection:</strong> Users which contributed to the projects show up in the author selection and can be filtered
+            by checking the respective boxes.
+          </li>
+          <li>
+            <strong>Category:</strong>
+            <br></br> Assignees - how many MergeRequests the user is assigned to as assignee.
+            <br></br> Reviewers - how many MergeRequests the user is assigned to as reviewer.
+          </li>
+          <li>
+            <strong>Only Show Authors:</strong> Only display user which have contributed to the project.
+          </li>
+        </ul>
       </div>
     );
   }
